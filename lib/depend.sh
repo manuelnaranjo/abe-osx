2ad42a73d29a7fad187c53a68cb1d8ceeb5251bb
Rob Savoye <rob.savoye@linaro.org>
2013-05-31 12:38:32 -0600

    load application specific configure flags so we use our freshly built libraries instead of system ones

--- lib/depend.sh
+++ lib/depend.sh
@@ -4,31 +4,52 @@
 # Build the dependencies needed to build the toolchain
 #
 
+depend()
+{
+    warning "unimplemented"
+
+    echo "FIXME depend(): ${depends}"
+}
+
+# These are the latest copies of the infrastructure files required to
+# fully build GCC in all it's glory. While it is possible to pass
+# --disable-* options at configure time to GCC, these are use for
+# good reason, so we download, build and install them.
 infrastructure()
 {
     fetch_http infrastructure/md5sums
 
     # Get the list of files from the md5sums list
     files="`cat ${local_snapshots}/infrastructure/md5sums | cut -d ' ' -f 3`"
+    if test -e "$(dirname "$0")/config/infrastructure.conf"; then
+	. "$(dirname "$0")/config/infrastructure.conf"
+    fi
+
+    if test x"${depends}" = x; then
+	error "No dependencies listed for inrastructure libraries!"
+	return 1
+    fi
+    
+    greparg="`echo ${depends} | tr ' ' '|'`"
+    files="`egrep " (ppl|isl|mpc|gmp|mpfr)" /linaro/src/linaro/cbuild/snapshots/infrastructure/md5sums | cut -d ' ' -f3`"
 
     # first fetch and extract all the tarballs listed in the md5sums file
     for i in ${files}; do
 	fetch_http infrastructure/$i
-	extract infrastructure/$i
+	extract infrastructure/$ig
 	name="`echo $i | sed -e 's:\.tar\..*::'`"
-    done
-
-    # Then configure as a separate step, so if something goes wrong, we
-    # at least have the sources
-    for i in ${files}; do
-	name="`echo $i | sed -e 's:\.tar\..*::'`"
-	configure infrastructure/${name} --disable-shared --enable-static --prefix=${PWD}/${hostname}/${build}/depends
-    done
-
-    # Finally compile and install the libaries
-    for i in ${files}; do
-	name="`echo $i | sed -e 's:\.tar\..*::'`"
-	build infrastructure/${name}
-	install infrastructure/${name}
+	# get any configure flags specific to this program, which are
+	# usually dependant libaries we've already built.
+	tool="`echo $i | sed -e 's:-[0-9].*::'`"
+	if test -e "$(dirname "$0")/config/${tool}.conf"; then
+	    . "$(dirname "$0")/config/${tool}.conf"
+	fi
+	configure infrastructure/${name} --disable-shared --enable-static --prefix=${PWD}/${hostname}/${build}/depends ${default_configure_flags}
+	# unset these two variables to avoid problems later
+	default_configure_flags=
+	depends=
+	make_all infrastructure/${name}
+	make_install infrastructure/${name}
     done
 }
+
