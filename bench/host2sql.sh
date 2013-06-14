fefa18dfb956364c731a21a5f6879faa287a7d9e
Rob Savoye <rob.savoye@linaro.org>
2013-06-14 12:59:06 -0600

    Parse additional fields for the newer schema. Separate hostdir from hostfile

--- bench/host2sql.sh
+++ bench/host2sql.sh
@@ -11,6 +11,21 @@ passwd="PASSWD"
 benchrun=0
 tmpdir="/tmp"
 
+if test x"$1" != x; then
+    hostdir=$1
+    hostfile=${hostdir}/host.txt
+else
+    hostdir=${PWD}
+    hostfile=${hostdir}/host.txt
+fi
+
+if test ! -e ${hostfile} -a ! -e ${hostdir}/benchmarks.txt; then
+    echo "ERROR: no data to parse!"
+    exit 1
+fi
+
+build_machine="`basename ${hostdir}| sed -e 's:^.*-::'`"
+
 # Check the environment for the database access information
 if test x"${CBUILD_DBUSER}" != x; then
     user="${CBUILD_DBUSER}"
@@ -23,21 +38,25 @@ fi
 benchrun="`mysql -uroot -pfu=br --local benchmark -e 'select count(*) from benchrun;' | tail -1 | tr -d '\n'`"
 benchrun="`expr ${benchrun} + 1`"
 
-date="`grep -A1 '^date:' host.txt | tail -1`"
+date="`grep -A1 '^date:' ${hostfile} | tail -1`"
+
 benchmark="EEMBC"
 base_result=
 status=		# enum('IDLE','RUNNING','DONE')
 enabled_cores=
 cores_per_chip=
 threads_per_core=
-target_arch="`grep "configured for a target of" host.txt | sed -e 's/^.*\`//' -e 's/.\.//'`"
-build_arch="`grep -A1 '^uname:' host.txt | tail -1 | cut -d ' ' -f 14`"
-enabled_chips="`grep '^processor' host.txt | wc -l`"
-gcc_version="`grep -A1 '^gcc:' host.txt | tail -1 | cut -d ' ' -f 2`"
-binutils_version="`grep -A1 '^as:' host.txt | tail -1 | cut -d ' ' -f 2`"
-libc_version="`grep -A1 '^ldd:' host.txt | tail -1 | cut -d ' ' -f 2-4`"
+target_arch="`grep "configured for a target of" ${hostfile} | sed -e 's/^.*\`//' -e 's/.\.//'`"
+build_arch="`grep -A1 '^uname:' ${hostfile} | tail -1 | cut -d ' ' -f 14`"
+build_os="`grep -A1 '^uname:' ${hostfile} | tail -1 | cut -d ' ' -f 3-4`"
+enabled_chips="`grep '^processor' ${hostfile} | wc -l`"
+codename="`grep '^Codename' ${hostfile} | cut -f 2`"
+build_gcc="`grep -A1 '^gcc:' ${hostfile} | tail -1 | cut -d ' ' -f 2`"
+binutils_version="`grep -A1 '^as:' ${hostfile} | tail -1 | cut -d ' ' -f 2`"
+libc_version="`grep -A1 '^ldd:' ${hostfile} | tail -1 | cut -d ' ' -f 2-4`"
 peak_result=
-ram="`grep '^Mem:' host.txt | cut -d ' ' -f 8`"
+ram="`grep '^Mem:' ${hostfile} | cut -d ' ' -f 8`"
+target_gcc="`grep mkdir ${hostdir}/toplevel.txt | head -1 | cut -d ' ' -f 3`"
 
 dump()
 {
@@ -47,11 +66,15 @@ dump()
     echo "Status:                     ${status}"
     echo "Target architcture:         ${target_arch}"
     echo "Build architecture:         ${build_arch}"
+    echo "Build machine:              ${build_machine}"
+    echo "Build os:                   ${build_os}"
+    echo "Codename:                   ${codename}"
     echo "Enabled Cores are:          ${enabled_cores}"
     echo "Enabled chips are:          ${enabled_chips}"
     echo "Core per chip are:          ${cores_per_chip}"
     echo "Threads per Core are:       ${threads_per_core}"
-    echo "GCC used for build is:      ${gcc_version}"
+    echo "GCC used for build is:      ${build_gcc}"
+    echo "Target GCC version is:      ${target_gcc}"
     echo "Binutils used for build is: ${binutils_version}"
     echo "Libc used for build is:     ${libc_version}"
     echo "Peak Result is:             ${peak_result}"
@@ -61,14 +84,18 @@ dump()
 
 # Produce an SQL formatted text file to import the data for this
 # benchmark run. This function only accesses the global data from
-# parsing the host.txt file.
+# parsing the ${hostfile} file.
 sql()
 {
     outfile="${tmpdir}/host.sql"
     rm -f ${outfile}
 
+    if test x"${target_arch}" = x; then
+	echo "ERROR: Couldn't get host config info for ${hostfile}"
+	exit
+    fi
     # Add header comment incase we ever have to edit this file manually
-    echo "# date | benchmark | base_result | status | target_arch | build_arch | enabled_cores | enabled_chips | cores_per_chip | threads_per_core | gcc_version | binutils_version | libc_version | peak_result | ram" > ${outfile}
+    echo "# date | benchmark | base_result | status | target_arch | build_arch | build_machine | build_os | codename | enabled_cores | enabled_chips | cores_per_chip | threads_per_core | gcc_version | binutils_version | libc_version | peak_result | ram" > ${outfile}
 
     echo -n "INSERT INTO benchrun VALUES " >> ${outfile}
 
@@ -78,11 +105,15 @@ sql()
     echo -n ",'${status}'"  >> ${outfile}
     echo -n ",'${target_arch}'"  >> ${outfile}
     echo -n ",'${build_arch}'"  >> ${outfile}
+    echo -n ",'${build_machine}'"  >> ${outfile}
+    echo -n ",'${build_os}'"  >> ${outfile}
+    echo -n ",'${codename}'"  >> ${outfile}
     echo -n ",'${enabled_cores}'"  >> ${outfile}
     echo -n ",'${enabled_chips}'"  >> ${outfile}
     echo -n ",'${cores_per_chip}'"  >> ${outfile}
     echo -n ",'${threads_per_core}'" >> ${outfile}
-    echo -n ",'${gcc_version}'" >> ${outfile}
+    echo -n ",'${build_gcc}'" >> ${outfile}
+    echo -n ",'${target_gcc}'" >> ${outfile}
     echo -n ",'${binutils_version}'" >> ${outfile}
     echo -n ",'${libc_version}'" >> ${outfile}
     echo -n ",'${peak_result}'" >> ${outfile}
@@ -95,5 +126,5 @@ sql()
 dump
 sql
 
-# Write the data into the banechmark.benchrun table
+# Write the data into the benchmark.benchrun table
 mysql -u${user} -p${passwd} --local benchmark < /${tmpdir}/host.sql
