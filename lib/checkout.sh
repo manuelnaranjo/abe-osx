60c62b363c893dce1679c2e20ea4fd275fb6135f
Rob Savoye <rob.savoye@linaro.org>
2013-07-22 14:39:37 -0600

    URLS for repositories now in config/sources.conf

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -4,46 +4,9 @@
 # This does a checkout from a source code repository
 #
 
-
-# branch gcc-4.6 svn://gcc.gnu.org/svn/gcc/branches/gcc-4_6-branch gcc-4.6
-# branch gcc-4.7 svn://gcc.gnu.org/svn/gcc/branches/gcc-4_7-branch gcc-4.7
-# branch gcc-4.8 svn://gcc.gnu.org/svn/gcc/branches/gcc-4_8-branch gcc-4.8
-# branch gcc-arm-aarch64-4.7 svn://gcc.gnu.org/svn/gcc/branches/ARM/aarch64-4.7-branch gcc-am-aarch64-4.7
-# branch gcc-arm-embedded-4.6 svn://gcc.gnu.org/svn/gcc/branches/ARM/embedded-4_6-branch gccarm-embedded-4.6
-# branch gcc-google-4.6 svn://gcc.gnu.org/svn/gcc/branches/google/gcc-4_6 gcc-google-4.6
-# branch gcc-trunk svn://gcc.gnu.org/svn/gcc/trunk gcc
-# 
 # svn+ssh://rsavoye@gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_8-branch
-# branch gcc-linaro/4.7 lp:gcc-linaro/4.8 gcc-linaro-4.8
-# branch gcc-linaro/4.7 lp:gcc-linaro/4.7 gcc-linaro-4.7
-# branch gcc-linaro/4.6 lp:gcc-linaro/4.6 gcc-linaro-4.6
-
-# branch gdb-linaro/7.6 lp:gdb-linaro/7.6 gdb-linaro-7.6
-# branch gdb-linaro/7.5 lp:gdb-linaro/7.5 gdb-linaro 7.5
-
-# git://git.linaro.org/toolchain/binutils.git
-# git://git.linaro.org/toolchain/glibc.git
-# git://git.linaro.org/toolchain/gdb.git
-# git://git.linaro.org/toolchain/newlib.git
-# ssh://robert.savoye@git.linaro.org/srv/git.linaro.org/git/toolchain/eglibc.git
-# ssh://robert.savoye@git.linaro.org/srv/git.linaro.org/git/toolchain/newlib.git newlib-linaro
-
-# branch crosstool-ng/linaro lp:~linaro-toolchain-dev/crosstool-ng/linaro crosstool-ng-linaro
-
-# branch cortex-strings lp:cortex-strings cortex-strings
-# branch boot-wrapper git://git.linaro.org/arm/models/boot-wrapper.git boot-wrapper
-
-# branch bitbake git://git.openembedded.org/bitbake bitbake
-# branch libav git://git.libav.org/libav.git libav
-# branch libffi git://github.com/atgreen/libffi.git libffi
-# branch llvm svn://llvm.org/svn/llvm-project/llvm/trunk llvm
-# branch meta-linaro git://git.linaro.org/openembedded/meta-linaro.git meta-linaro
-# branch openembedded-core git://git.openembedded.org/openembedded-core openembedded-core
-# branch valgrind svn://svn.valgrind.org/valgrind/trunk valgrind
-# branch qemu-git git://git.qemu.org/qemu.git qemu
-# branch qemu-linaro git://git.linaro.org/qemu/qemu-linaro.git qemu-linaro
-# ssh://git.linaro.org/srv/git.linaro.org/git/qemu/qemu-linaro.git qemu-linaro
-# git submodule update --init roms
+# eglibc ssh://robert.savoye@git.linaro.org/srv/git.linaro.org/git/toolchain/eglibc.git
+# newlib-linaro ssh://robert.savoye@git.linaro.org/srv/git.linaro.org/git/toolchain/newlib.git
 
 # It's optional to use git-bzr-ng or git-svn to work on the remote sources,
 # but we also want to work with the native surce code control system.
@@ -58,11 +21,10 @@ checkout()
 	return 1
     fi
 
-    # bzr uses slashes in it's path names, so convert them so we
-    # can use the for creating the source directory.
     dir="`normalize_path $1`"
 
-    # We use git for our copy by importing from the other systems
+    notice "Checking out sources for $1"
+
     case $1 in
 	bzr*|lp*)
 	    if test x"${force}" =  xyes; then
@@ -79,7 +41,7 @@ checkout()
 		fi
 	    fi
 	    ;;
-	svn*)
+	svn*|http*)
 	    trunk="`echo $1 |grep -c trunk`"
 	    if test ${trunk} -gt 0; then
 		dir="`dirname $1`"
