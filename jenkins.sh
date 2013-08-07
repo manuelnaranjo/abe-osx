f2816f78283a266c66d183b303eae5ab64e9356e
Rob Savoye <rob.savoye@linaro.org>
2013-08-07 16:21:43 -0600

    new script for Jenkins to fire up a build.

---
+++ jenkins.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Test the config parameters from the Jenkins Build Now page
+if test x"${gmp_snapshot}" != x"latest"; then
+    change="${change} ${gmp_snapshot}"
+fi
+
+if test x"${mpc_snapshot}" != x"latest"; then
+    change="${change} ${mpc_snapshot}"
+fi
+if test x"${mpfr_snapshot}" != x"latest"; then
+    change="${change} ${mpfr_snapshot}"
+fi
+if test x"${gcc_snapshot}" != x"latest"; then
+    change="${change} ${gcc_snapshot}"
+fi
+if test x"${binutils_snapshot}" != x"latest"; then
+    change="${change} ${binutils_snapshot}"
+fi
+if test x"${linux_snapshot}" != x"latest"; then
+    change="${change} ${liux_snapshot}"
+fi
+# if test x"${runtests}" != x"latest"; then
+# fi
+
+mkdir -p _build
+echo "Current working top level directory: $PWD"
+cd _build
+rm -f localhost/${target}/*/*.conf
+../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
+export CONFIG_SHELL="/bin/bash"
+$CONFIG_SHELL ../cbuild2.sh --force --parallel ${changes} --target $target --build all
