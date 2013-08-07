dc521d8fea503c960a04e70b8532eb1a27fd204d
Rob Savoye <rob.savoye@linaro.org>
2013-08-07 16:26:19 -0600

    add toolname to set the parameter right

--- jenkins.sh
+++ jenkins.sh
@@ -3,31 +3,37 @@
 # Test the config parameters from the Jenkins Build Now page
 changes=""
 if test x"${gmp_snapshot}" != x"latest"; then
-    change="${change} ${gmp_snapshot}"
+    change="${change} gmp=${gmp_snapshot}"
 fi
 
 if test x"${mpc_snapshot}" != x"latest"; then
-    change="${change} ${mpc_snapshot}"
+    change="${change} mpc=${mpc_snapshot}"
 fi
 if test x"${mpfr_snapshot}" != x"latest"; then
-    change="${change} ${mpfr_snapshot}"
+    change="${change} mpfr=${mpfr_snapshot}"
 fi
 if test x"${gcc_snapshot}" != x"latest"; then
-    change="${change} ${gcc_snapshot}"
+    change="${change} gcc=${gcc_snapshot}"
 fi
 if test x"${binutils_snapshot}" != x"latest"; then
-    change="${change} ${binutils_snapshot}"
+    change="${change} binutils=${binutils_snapshot}"
 fi
 if test x"${linux_snapshot}" != x"latest"; then
-    change="${change} ${liux_snapshot}"
+    change="${change} linux-${linux_snapshot}"
 fi
 # if test x"${runtests}" != x"latest"; then
 # fi
 
 mkdir -p _build
-echo "Current working top level directory: $PWD"
 cd _build
+# Delete all local config files, so any rebuilds use the currently
+# committed versions.
 rm -f localhost/${target}/*/*.conf
-../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
+
+# Configure Cbuildv2 itself. Force the use of bash instead of the Ubuntu
+# default of dash as some configure scripts go into an infinite loop with
+# dash. Not good...
+export CONFIG_SHELL="/bin/bash"
+$CONFIG_SHELL ../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
 export CONFIG_SHELL="/bin/bash"
 $CONFIG_SHELL ../cbuild2.sh --force --parallel ${change} --target $target --build all
