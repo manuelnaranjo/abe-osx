62ed84aeae34e0529af71d2177311dc9f075e39b
Rob Savoye <rob.savoye@linaro.org>
2013-08-15 17:53:45 -0600

    get the build machine

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Test the config parameters from the Jenkins Build Now page
+
+# Get the versions of dependant components to use
 changes=""
 if test x"${gmp_snapshot}" != x"latest"; then
     change="${change} gmp=${gmp_snapshot}"
@@ -21,6 +23,60 @@ fi
 if test x"${linux_snapshot}" != x"latest"; then
     change="${change} linux-${linux_snapshot}"
 fi
+
+# Get the build machine architecture
+case ${build_machine} in 
+    toolchain_cloud)
+	;;
+    all_native)
+	;;
+    all_cross)
+	a9-builder)
+	;;
+    a9-daily)
+	;;
+    a9-ref)
+	;;
+    a9hf-builder)
+	;;
+    a9hf-daily)
+	;;
+    a9hf-ref)
+	;;
+    armv5-builder)
+	;;
+    armv6-ref)
+	;;
+    i686)
+	;;
+    i686-lucid)
+	;;
+    lava-calxeda)
+	;;
+    lava-panda-mock)
+	;;
+    lava-panda-usbdrive)
+	;;
+    lava-pandaes)
+	;;
+    lava-pandaes-usbdrive) 
+	;;
+    lava-qemu-minimal)
+	;;
+    x86_64)
+	;;
+    x86_64-heavy)
+	;;
+    xaarch64)
+	;;
+    xaarch64_bare)
+	;;
+    xcortexa15hf)
+	;;
+    *)
+	;;
+ esac
+
 # if test x"${runtests}" != x"latest"; then
 # fi
 
@@ -35,5 +91,6 @@ rm -f localhost/${target}/*/*.conf
 # dash. Not good...
 export CONFIG_SHELL="/bin/bash"
 $CONFIG_SHELL ../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
-export CONFIG_SHELL="/bin/bash"
+
+# Run Cbuildv2. We force all components to rebuild cleanly, and do parallel builds.
 $CONFIG_SHELL ../cbuild2.sh --force --parallel ${change} --target $target --build all
