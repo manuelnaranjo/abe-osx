ce612d0204aadb3c2eeb90036b8ccb047ccc3a3c
Rob Savoye <rob.savoye@linaro.org>
2013-08-15 21:13:28 -0600

    support for building binary packages

---
+++ lib/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script contains functions for building binary packages.
+binary_tarball()
+{
+    echo "unimplemtned"
+}
+
+build_deb()
+{
+    echo "unimplemtned"
+}
+
+build_rpm()
+{
+    echo "unimplemtned"
+}
