f673e78cfeca5daee777bd537428a60d73446c83
Rob Savoye <rob.savoye@linaro.org>
2013-08-28 16:40:05 -0600

    implement testcode function

--- lib/testcode.sh
+++ lib/testcode.sh
@@ -4,5 +4,20 @@
 
 testcode()
 {
-    notice "uninplemented"
+    # force downloading the md5sum file, as it changes frequently
+    rm -f ${local_snapshots}/testcode/md5sums
+    fetch_http testcode/md5sums
+
+    if test -f ${local_snapshots}/testcode/md5sums; then
+     	files="`cat ${local_snapshots}/testcode/md5sums | cut -d ' ' -f3`"
+     	for i in ${files}; do
+     	    build testcode/$i
+	    if test $? -gt 0; then
+		error "Couldn't build testcode/$i!"
+		return 1
+	    fi
+     	done
+    fi
+   
 }
+
