a975aa3a02ffef57ed48f8692a99faa2e78872b9
Rob Savoye <rob.savoye@linaro.org>
2013-09-18 16:33:22 -0600

    make tarballs optional with parameter, fix importing test results into Jenkins.

--- jenkins.sh
+++ jenkins.sh
@@ -4,27 +4,24 @@
 
 # Get the versions of dependant components to use
 changes=""
-if test x"${gmp_snapshot}" != x"latest"; then
+if test x"${gmp_snapshot}" != x"latest" -a x"${gmp_snapshot}" != x; then
     change="${change} gmp=${gmp_snapshot}"
 fi
-if test x"${mpc_snapshot}" != x"latest"; then
+if test x"${mpc_snapshot}" != x"latest" -a x"${mpc_snapshot}" != x; then
     change="${change} mpc=${mpc_snapshot}"
 fi
-if test x"${mpfr_snapshot}" != x"latest"; then
+if test x"${mpfr_snapshot}" != x"latest" -a x"${mpfr_snapshot}" != x; then
     change="${change} mpfr=${mpfr_snapshot}"
 fi
-if test x"${gcc_snapshot}" != x"latest"; then
+if test x"${gcc_snapshot}" != x"latest" -a x"${gcc_snapshot}" != x; then
     change="${change} gcc=${gcc_snapshot}"
 fi
-if test x"${binutils_snapshot}" != x"latest"; then
+if test x"${binutils_snapshot}" != x"latest" -a x"${binutils_snapshot}" != x; then
     change="${change} binutils=${binutils_snapshot}"
 fi
-if test x"${linux_snapshot}" != x"latest"; then
+if test x"${linux_snapshot}" != x"latest" -a x"${linux_snapshot}" != x; then
     change="${change} linux-${linux_snapshot}"
 fi
-
-# Delete the previous build directory
-# rm -fr _build
 
 # Create a build directory
 mkdir -p _build
@@ -43,11 +40,16 @@ export CONFIG_SHELL="/bin/bash"
 if test x"${debug}" = x"true"; then
     export CONFIG_SHELL="/bin/bash -x"
 fi
-$CONFIG_SHELL ../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
+
+#$CONFIG_SHELL ../configure --with-local-snapshots=$WORKSPACE/cbuildv2/snapshots
 
 # if runtests is true, then run make check after the build completes
 if test x"${runtests}" = xtrue; then
     runtest=--check
+fi
+
+if test x"${bootstrap}" = xtrue; then
+    tarballs=--tarballs
 fi
 
 # if build_type is true, then this is a cross build. For cross builds we build a
@@ -56,16 +58,17 @@ if test x"${bootstrap}" = xtrue; then
     $CONFIG_SHELL ../cbuild2.sh --nodepends --parallel ${change} --disable bootstrap --build all
 fi
 
-$CONFIG_SHELL ../cbuild2.sh --nodepends --parallel ${change} ${runtest} --tarballs --target ${target} --build all
+$CONFIG_SHELL ../cbuild2.sh --nodepends --parallel ${change} ${runtest} ${tarballs} --target ${target} --build all
 
 if test $? -eq 0; then
     if test x"${runtests}" = xtrue; then
-	sums="`find -name \*.sum`"
+	sums="`find -name *.sum`"
 	for i in ${sums}; do
 	    name="`echo $i | cut -d '.' -f 1`"
-	    ../sum2junit.sh $i ${name}.junit
+	    ../sum2junit.sh $i
 	done
-	cat *.junit
-	rm *.junit
+	junits="`find -name *.junit`"
+	cat ${junits}
+	rm ${junits}
     fi
 fi
