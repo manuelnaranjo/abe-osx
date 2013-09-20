104e92ffd3ca187fc5b393a56f7adec103442176
Rob Savoye <rob.savoye@linaro.org>
2013-09-20 13:10:53 -0600

    add tests for get_srcdir(). Add support for an untested result, as these tests require directories to be pre-existing

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -42,6 +42,13 @@ pass()
     passes="`expr ${passes} + 1`"
 }
 
+untested=0
+untested()
+{
+    echo "UNTESTED: $1"
+    untested="`expr ${untested} + 1`"
+}
+
 failures=0
 fail()
 {
@@ -55,6 +62,9 @@ totals()
     echo "Total test results:"
     echo "	Passes: ${passes}"
     echo "	Failures: ${failures}"
+    if test ${untested} -gt 0; then
+	echo "	Untested: ${untested}"
+    fi
 }
 
 #
@@ -331,6 +341,63 @@ if test x"${out}" = x"git://git.linaro.org/toolchain/newlib.git"; then
 else
     fail "get_source: git repository with branch and commit"
     fixme "get_source returned ${out}"
+fi
+dryrun=no
+
+# ----------------------------------------------------------------------------------
+
+# FIXME: Note these following test cases only PASS if you have the source
+# directories created already.
+if test -d ${local_snapshots}/gcc.git; then
+    in="git://git.linaro.org/toolchain/gcc.git"
+    out="`get_srcdir $in | grep -v TRACE`"
+    if test x"${out}" = x"${local_snapshots}/gcc.git"; then
+	pass "get_srcdir: git repository"
+    else
+	fail "get_srcdir: git repository"
+	fixme "get_srcdir returned ${out}"
+    fi
+else
+    untested  "get_srcdir: git repository"
+fi
+
+if test -d ${local_snapshots}/gcc.git-linaro-4.8-branch; then
+    in="git://git.linaro.org/toolchain/gcc.git/linaro-4.8-branch"
+    out="`get_srcdir $in | grep -v TRACE`"
+    if test x"${out}" = x"${local_snapshots}/gcc.git-linaro-4.8-branch/gcc-4_8-branch"; then
+	pass "get_srcdir: git repository with branch"
+    else
+	fail "get_srcdir: git repository with branch"
+	fixme "get_srcdir returned ${out}"
+    fi
+else
+    untested "get_srcdir: git repository with branch"
+fi
+
+if test -d ${local_snapshots}/gcc.git-linaro-4.8-branch@123456; then
+    in="git://git.linaro.org/toolchain/gcc.git/linaro-4.8-branch@123456"
+    out="`get_srcdir $in | grep -v TRACE`"
+    if test x"${out}" = x"${local_snapshots}/gcc.git-linaro-4.8-branch@123456/gcc-4_8-branch"; then
+	pass "get_srcdir: git repository with branch and commit"
+    else
+	fail "get_srcdir: git repository with branch and commit"
+	fixme "get_srcdir returned ${out}"
+    fi
+else
+    untested "get_srcdir: git repository with branch and commit"
+fi
+
+if test -d ${local_snapshots}/gcc-linaro-4.8-2013.06-1; then
+    in="gcc-linaro-4.8-2013.06-1.tar.xz"
+    out="`get_srcdir $in | grep -v TRACE`"
+    if test x"${out}" = x"${local_snapshots}/gcc-linaro-4.8-2013.06-1"; then
+	pass "get_srcdir: with tarball"
+    else
+	fail "get_srcdir: with tarball"
+	fixme "get_srcdir returned ${out}"
+    fi
+else
+    untested "get_srcdir: with tarball"
 fi
 
 # list of dependencies for a toolchain component
