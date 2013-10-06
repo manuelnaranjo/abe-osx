3939688b1aa2249e0918faf0ab93cad1938d268f
Rob Savoye <rob.savoye@linaro.org>
2013-10-06 09:53:11 -0600

    add release_gcc_src function, make source releases match official releases

--- lib/release.sh
+++ lib/release.sh
@@ -6,88 +6,156 @@
 # everything necesary for the release.
 #
 
-# Get the tool name, which we use to differenciate between the various
-# release processes.
-get_tool()
+# Regenerate the MD5SUMS file
+regenerate_checksums()
 {
-    # tool names look like gdb-linaro-VERSION
-    tool="`echo $1 | sed -e 's:-[0-9].*::'`"
-    case ${tool} in
-	eglibc*)
-	    tool=eglibc
-	    ;;
-	binutils*)
-	    tool=binutils
-	    ;;
-	newlib*)
-	    tool=newlib
-	    ;;
-	gdb*)
-	    tool=gdb
-	    release_gdb
-	    ;;
-	gcc*)
-	    tool=gcc
-	    release_gcc
-	    ;;
-	qemu*)
-	    tool=qemu
-	    ;;
-	meta-linaro*)
-	    tool=meta
-	    ;;
-	*)
-	    tool=gcc
-	    ;;
-    esac
+    trace "$*"
 
-    return ${tool}
+    local reldir=$1
+
+    local tag="`create_release_tag $1`"
+
+    cat <<EOF > ${reldir}/MD5SUMS
+# This file contains the MD5 checksums of the files in the 
+# gcc-linaro-${tag}.tar.xz tarball.
+#
+# Besides verifying that all files in the tarball were correctly expanded,
+# it also can be used to determine if any files have changed since the
+# tarball was expanded or to verify that a patchfile was correctly applied.
+#
+# Suggested usage:
+# md5sum -c MD5SUMS | grep -v "OK$"
+EOF
+
+    rm -f ${reldir}/MD5SUMS
+    touch ${reldir}/MD5SUMS
+ 
+    find ${reldir}/ -type f | grep -v MD5SUMS | sort > /tmp/md5sums
+    for i in `cat /tmp/md5sums`; do
+	md5sum $i 2>&1 | sed -e 's:/tmp/::' >> ${reldir}/MD5SUMS
+    done
+
+    return 0
+}
+
+# GPG sign the tarball
+sign_tarball()
+{
+    trace "$*"
+
+#    ssh -t cbuild@toolchain64.lab gpg --no-use-agent -q --yes --passphrase-file /home/cbuild/.config/cbuild/password --armor --sign --detach-sig --default-key cbuild "/home/cbuild/var/snapshots/gcc-linaro-${release}.tar.xz" scp cbuild@toolchain64.lab:/home/cbuild/var/snapshots/gcc-linaro-${release}.tar.xz.asc $REL_DIR
+
+    return 0
 }
 
 release()
 {
-    tool="`get_tool $1`"
+    trace "$*"
+
+    local tool="`get_toolname $1`"
     notice "Releasing ${tool}"
     release_gdb $1		# FIXME: don't hardcode the tool name!
 }
 
-# Edit the ChangeLog.linaro file for this release
-edit_changelog()
+# From: https://wiki.linaro.org/WorkingGroups/ToolChain/GCC/ReleaseProcess
+# $1 - 
+release_gcc_src()
 {
-    # bzr uses slashes in it's path names, so convert them so we
-    # can use the for accessing the source directory.
-    url="`echo $1 | sed -e 's:/:_:'`"
-    dir="`basename ${url} |sed -e 's/^.*://'`"
+    trace "$*"
 
-    clog="${local_snapshots}/${dir}/ChangeLog.linaro"
-    # Edit ChangeLog.linaro and add "GDB Linaro 7.X-20XX.XX[-X] released."
-    if test ! -e ${clog}; then
-	warning "${clog} doesn't exist, so creating it"
-	touch ${clog}
+    # See if specific component versions were specified at runtime
+    if test x"${gcc_version}" = x; then
+	local gcc_version="`grep ^latest= ${topdir}/config/gcc.conf | cut -d '\"' -f 2` | tr -d '\"'"
+    fi
+    local srcdir="`get_srcdir ${gcc_version}`"
+    local builddir="`get_builddir ${gcc_version}`"
+    local tag="`create_release_tag ${gcc_version}`"
+    local destdir=/tmp/${tag}
+
+    install_gcc_docs ${destdir} ${srcdir} ${builddir}
+
+    # Update the GCC version
+    rm -f ${destdir}/gcc/LINARO-VERSION
+    if test x"${release}" = x;then
+	echo "${tag}" > ${destdir}/gcc/LINARO-VERSION
+	edit_changelogs ${srcdir} ${tag}
+    else
+	echo "${release}" > ${destdir}/gcc/LINARO-VERSION
+	edit_changelogs ${srcdir} ${release}
+    fi
+    
+    regenerate_checksums ${destdir}
+
+    # Remove extra files left over from any development hacking
+    sanitize ${srcdir}
+
+    # make a link with the correct name for the tarball's source directory
+    dryrun "ln -sfnT ${srcdir} /tmp/${tag}"
+    
+    dryrun "tar Jcvfh ${local_snapshots}/${tag}.tar.xz --directory=/tmp --exclude .git ${tag}/"
+
+    # Make the md5sum file for this tarball
+    rm -f ${local_snapshots}/${tag}.tar.xz.asc
+    dryrun "md5sum ${local_snapshots}/${tag}.tar.xz > ${local_snapshots}/${tag}.tar.xz.asc"
+
+    return 0
+}
+
+# At release time, we build additional docs
+# $1 - 
+install_gcc_docs()
+{
+    trace "$*"
+
+    local destdir=$1
+    local srcdir=$2
+    local builddir=$3
+
+    if test ! -d ${destdir}; then
+	dryrun "ln -sfnT ${srcdir} ${destdir}"
     fi
 
-    year="`date +%Y`"
-    month="`date +%m`"
-    day="`date +%d`"
+    # Create the html docs for the INSTALL directory
+
+    # the GCC script needs these two values to work.
+    SOURCEDIR=${srcdir}/gcc/doc
+    DESTDIR=${destdir}/INSTALL
+    . ${srcdir}/gcc/doc/install.texi2html
+    
+#    dryrun "cp -f ${builddir}/gcc/po/*.gmo ${destdir}/gcc/po/"
+        
+    # Copy all the info files and man pages into the release directory
+    local docs="`find ${builddir}/ -name \*.info -o -name \*.1 -o -name \*.7 | sed -e "s:${builddir}/::"`"
+    for i in ${docs}; do
+      	dryrun "cp ${builddir}/$i ${destdir}/$i"
+    done
+
+    return 0
+}
+
+# Edit the ChangeLog.linaro file for this release
+edit_changelogs()
+{
+    trace "$*"
 
     if test x"${fullname}" = x; then
 	case $1 in
 	    bzr*|lp*)
 	    # Pull the author and email from bzr whoami
-		fullname="`bzr whoami | sed -e 's: <.*::'`"
-		email="`bzr whoami --email`"
+		local fullname="`bzr whoami | sed -e 's: <.*::'`"
+		local email="`bzr whoami --email`"
 		;;
 	    svn*)
-		trunk="`echo $1 |grep -c trunk`"
+		local trunk="`echo $1 |grep -c trunk`"
 		if test ${trunk} -gt 0; then
-		    dir="`dirname $1`"
-		    dir="`basename ${dir}`/trunk"
+		    local dir="`dirname $1`"
+		    local dir="`basename ${dir}`/trunk"
 		fi
 		;;
 	    git*)
 		if test -f ~/.gitconfig; then
-		    fullname="`grep "name = " ~/.gitconfig | cut -d ' ' -f 3-6`"
-		    email="`grep "email = " ~/.gitconfig | cut -d ' ' -f 3-6`"
+		    local fullname="`grep "name = " ~/.gitconfig | cut -d ' ' -f 3-6`"
+		    local email="`grep "email = " ~/.gitconfig | cut -d ' ' -f 3-6`"
 		fi
 		;;
 	    *)
@@ -95,54 +163,48 @@ edit_changelog()
 	esac
     fi
 
-    if test x"$2" != x; then
-	respin=-$2
-	nextspin=$(($2 + 1))
-    else
-	respin=
-	nextspin=1
+    local date="`date +%Y-%m-%d`"
+
+    local clogs="`find $1 -name ChangeLog.linaro`"
+    if test x"${clogs}" = x; then
+	local clogs="`find $1 -type d`/ChangeLog.linaro"
     fi
-    series="`echo $1 | sed -r 's#.+/(.+)#\1#'`"
-    release=${year}.${month}${respin}
 
-    mv ${clog} /tmp/ChangeLog.linaro
-
-    #tool="`get_tool $1`"
-    tool=gdb
-    case ${tool} in
-	gdb)
-	    cat >> ${clog} <<EOF
-${year}-${month}-${day}  ${fullname}  <${email}>
-        GDB Linaro ${series}-${release} released.
-
-        gdb/
-        * version.in: Update.
-
-EOF
-	    echo "gdb/" >> ${clog}
-	    echo "  * version.in: Update" >> ${clog}
-	    ;;
-	gcc)
-	    cat >> ${clog} <<EOF
-${year}-${month}-${day}  ${fullname}  <${email}>
-        GCC Linaro ${series}-${release} released.
-
-        gcc/
-        * LINARO-VERSION: Update.
-
-EOF
-	    echo "gcc/" >> ${clog}
-	    ;;
-    esac
-
-    # tack the original ChangeLog onto the new version with out update.
-    cat /tmp/ChangeLog.linaro >> ${clog}   
+    if test `echo ${srcdir} | grep -c "/gdb"`; then
+	local tool=gdb
+	if test `echo ${srcdir} | grep -c "/gcc"`; then
+	    local tool=gdb
+	    if test `echo ${srcdir} | grep -c "/binutils"`; then
+		local tool=binutils
+		if test `echo ${srcdir} | grep -c "/glibc"`; then
+		    local tool=glibc
+		    if test `echo ${srcdir} | grep -c "/eglibc"`; then
+			local tool=eglibc
+			if test `echo ${srcdir} | grep -c "/newlib"`; then
+			    local tool=newlib
+			fi
+		    fi
+		fi
+	    fi
+	fi
+    fi
+    for i in ${clogs}; do
+	mv $i /tmp/
+	echo "${date}  ${fullname}  <${email}>" >> $i
+	echo "" >> $i
+        echo "        GCC Linaro $2 released." >> $i
+	echo "" >> $i
+	cat /tmp/ChangeLog.linaro >> $i
+	rm /tmp/ChangeLog.linaro
+    done
 }
 
 # From: https://wiki.linaro.org/WorkingGroups/ToolChain/GDB/ReleaseProcess
 # $1 - file name-version to grab from source code control.
 release_gdb()
 { 
+    trace "$*"
+
     # First, checkout all the sources
     if test -d ${local_snapshots}/$1; then
 	checkout $1 
@@ -157,16 +219,6 @@ release_gdb()
     # Check in the changes, and tag them
     # bzr commit -m "Make 7.X-20XX.XX[-X] release."
     # bzr tag gdb-linaro-7.X-20XX.XX[-X]
-}
-
-# From: https://wiki.linaro.org/WorkingGroups/ToolChain/GCC/ReleaseProcess
-release_gcc()
-{
-    # First, checkout all the sources, or grab a snapshot
-
-    Update every ChangeLog.linaro file.
-    date="`date +%Y-%m-%d`"
-    # release=$(cut -f1,2 -d. $REL_DIR/svn/gcc/BASE-VER)-$(date +%Y.%m)
 }
 
 release_binutils()
@@ -202,9 +254,9 @@ release_qemu()
 tag_release()
 {
     if test x"$1" = x; then
-	dccs=git
+	local dccs=git
     else
-	dccs = $1
+	local dccs = $1
     fi
     error "release TAGging unimplemented"
 }
