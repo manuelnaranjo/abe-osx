2863a2e42b2c6e0360a4ec2f9dec4ba7d855a980
Ryan S. Arnold <ryan.arnold@linaro.org>
2013-11-22 14:54:49 -0600

    Add documentation for git-parser.sh.

--- lib/git-parser.sh
+++ lib/git-parser.sh
@@ -1,5 +1,83 @@
 #!/bin/sh
 
+# The get_git_<part> functions all operate on the same premise:
+#
+#    When given a string that represents a git url or git repository
+#    identifier (plus branch and/or revision information) they will
+#    parse the URL for the requested part.
+#
+# Forms:
+#
+#   get_git_service
+#      The valid git services are: 'git' and 'http'.
+#
+#   get_git_user
+#      Valid usernames are: 'username' and 'multi.part.username".
+#
+#   get_git_repo
+#      The name of the respository including the '.git' suffix.
+#
+#   get_git_tool
+#      The same as 'repo' but minus the '.git' suffix.
+#
+#   get_git_url
+#       The full url of the repository, minus branch and/or revision
+#       information.  This is valid for passing to git.
+#
+#   get_git_branch
+#       The branch designation that follows the repository, e.g.,
+#       binutils.git/branch
+#
+#   get_git_revision
+#       The revision designation that follows the repository, e.g.,
+#       binutils.git@12345
+#
+#         Note: cbuild allows <repo>.git/branch@revision even though
+#               when a revision is present, the 'branch' is only used
+#               in path names, since a revision implies a branch already.
+#
+# Calling convention:
+#
+#   Because the get_git_<part> functions return a string in stdout
+#   they must be called in a subshell using the following convention:
+#
+#   local out=
+#   out="`get_git_<part> <input_string>`"
+#   if test $? -gt 1; then
+#       # Parser detected malformed input.  Depending on what
+#       # you expect, this might or might-not be an error.
+#   elif test x"${out}" = x; then
+#       # Parser didn't parse the requested part because
+#       # it probably wasn't in the string OR the input
+#       # is malformed.
+#   fi
+#
+# Return Value
+#   stdout: Returns the requested string (if parsed)
+#   $?:     Returns the error status code
+#   stderr: If $? != 0 then this contains information about
+#           malformed input.
+#
+# Input String:
+#   Valid inputs:
+#      [{git|http}://[<username>@]{<url.foo>|127.0.0.1}/path/]<repo>.git[/<branch>][@<revision]
+#
+#   A full git url with branch and revision information, e.g.,
+#
+#      http://firstname.lastname@staging.git.linaro.org/git/toolchain/gcc.git/linaro_4.9_branch@12345
+#
+#   A git repo identifier with branch and revision information:
+#
+#      gcc.git/linaro_4.9_branch@12345
+#
+# Examples:
+#
+#   For examples please see testsuite/git-parser-tests.sh
+#
+
+# This is the internal unified parser function.
+# DO NOT USE THIS FUNCTION DIRECTLY.
+# Use the get_git_<part> functions.
 git_parser()
 {
     local part=$1
@@ -19,13 +97,7 @@ git_parser()
     # Set to '1' if something in ${in} is malformed.
     local err=0
 
-    #local service="`echo ${in} | sed -e 's#\(^git\)://.*#\1#;s#\(^http\)://.*#\1#'`"
     local service="`echo ${in} | sed -n 's#\(^git\)://.*#\1#p;s#\(^http\)://.*#\1#p'`"
-
-    if test x"${service}" = x; then
-	error "Malformed input.  No service defined."
-	err=1
-    fi
 
     # This will only find a 'username' if it follows the <service>://
     # and precededs the first . and or / in the url.  Yes you could
