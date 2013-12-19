315cc7373fa1883b718da73cb862b9a2fd614b35
Rob Savoye <rob.savoye@linaro.org>
2013-12-19 16:52:14 -0700

    more path changes to work in a chroot
    Change-Id: I9ff4cfacd1686411b7012dddcdab867e8181f3a4

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,8 @@
 
 # Test the config parameters from the Jenkins Build Now page
 
+topspace=`dirname ${WORKSPACE}`
+
 if test x"${tarsrc}" = xtrue; then
     release="--tarsrc"
 fi
@@ -9,7 +11,7 @@ fi
 # chroot, since all our builds machines are 64bit.
 if test x"${tarbin}" = xtrue; then
     release="${release} --tarbin "
-    schroot="schroot --chroot=lucid --directory=${WORKSPACE} --preserve-environment "
+    schroot="schroot --chroot=lucid --directory=${topspace} --preserve-environment "
 # --user=buildslave
 else
     schroot=""			# FIXME: eval ??
@@ -39,12 +41,12 @@ fi
 # Remove the previous build if specified, default to reusing the existing
 # build directory.
 if test x"${reuse}" != x"true"; then
-    rm -fr ${WORKSPACE}/_build
+    rm -fr ${topspace}/_build
 fi
 
 # Create a build directory
-if test ! -d ${WORKSPACE}/_build; then
-    mkdir -p ${WORKSPACE}/_build
+if test ! -d ${topspace}/_build; then
+    mkdir -p ${topspace}/_build
 fi
 
 # Use the newly created build directory
@@ -62,7 +64,7 @@ if test x"${debug}" = x"true"; then
     export CONFIG_SHELL="/bin/bash -x"
 fi
 
-$CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/configure --with-local-snapshots=$WORKSPACE/shared/snapshots
+$CONFIG_SHELL ${topspace}/shared/cbuildv2/configure --with-local-snapshots=$WORKSPACE/shared/snapshots
 
 # if runtests is true, then run make check after the build completes
 if test x"${runtests}" = xtrue; then
@@ -73,15 +75,15 @@ fi
 # that to compile the cross compiler to bootstrap. Since it's just
 # used to build the cross compiler, we don't bother to run 'make check'.
 if test x"${bootstrap}" = xtrue; then
-    $CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} --build all
+    $CONFIG_SHELL ${topspace}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} --build all
 fi
 
 # Now we build the cross compiler, for a native compiler this becomes
 # the stage2 bootstrap build.
-$CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${check} ${release} --target ${target} --build all
+$CONFIG_SHELL ${topspace}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${check} ${release} --target ${target} --build all
 
 # Create the BUILD-INFO file for Jenkins.
-cat << EOF > ${WORKSPACE}/BUILD-INFO.txt
+cat << EOF > ${topspace}/BUILD-INFO.txt
 Format-Version: 0.5
 
 Files-Pattern: *
@@ -95,14 +97,14 @@ rm -f ${WORKSPACE}/*.junit 2>&1 > /dev/null
 # convert to JUNIT format, which is what Jenkins wants it's results
 # in. We then cat them to the console, as that seems to be the only
 # way to get the results into Jenkins.
-sums="`find $WORKSPACE -name *.sum`"
+sums="`find ${topspace} -name *.sum`"
 if test x"${sums}" != x; then
     echo "Found test results finally!!!"
     for i in ${sums}; do
 	name="`basename $i`"
-	${WORKSPACE}/shared/cbuildv2/sum2junit.sh $i $WORKSPACE/${name}.junit
+	${topspace}/shared/cbuildv2/sum2junit.sh $i $WORKSPACE/${name}.junit
     done
-    junits="`find $WORKSPACE/ -name *.junit`"
+    junits="`find ${WORKSPACE} -name *.junit`"
     if test x"${junits}" != x; then
 	echo "Found junit files finally!!!"
     else
@@ -115,15 +117,15 @@ fi
 # Canadian Crosses are a win32 hosted cross toolchain built on a Linux
 # machine.
 if test x"${canadian}" = x"true"; then
-    $CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} --target ${target} --build all
+    $CONFIG_SHELL ${topspace}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} --target ${target} --build all
     distro="`lsb_release -sc`"
     # Ubuntu Lucid uses an older version of Mingw32
     if test x"${distro}" = x"lucid"; then
-	$CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${release} --host=i586-mingw32msvc --target ${target} --build all
+	$CONFIG_SHELL ${topspace}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${release} --host=i586-mingw32msvc --target ${target} --build all
     else
-	$CONFIG_SHELL ${WORKSPACE}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${release} --host=i686-w64-mingw32 --target ${target} --build all
+	$CONFIG_SHELL ${topspace}/shared/cbuildv2/cbuild2.sh --nodepends --parallel ${change} ${release} --host=i686-w64-mingw32 --target ${target} --build all
     fi
 fi
 
-touch $WORKSPACE/cbuildv2/*.junit
+touch $WORKSPACE/*.junit
 
