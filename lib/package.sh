1a0532a88012ae02aaeb1a5c3e32ff3d1c0da48a
Rob Savoye <rob.savoye@linaro.org>
2014-02-04 16:16:10 -0700

    install gdbserver into the GDB binary release
    Change-Id: I15cd53ac36b3a7a4cd8d19169c626102e0a52d1d

--- lib/package.sh
+++ lib/package.sh
@@ -127,6 +127,7 @@ binary_gdb()
     # install in alternate directory so it's easier to build the tarball
     dryrun "make all ${make_flags} DESTDIR=${destdir} -w -C ${builddir}"
     dryrun "make install ${make_flags} DESTDIR=${destdir} -w -C ${builddir}"
+    dryrun "make install ${make_flags} DESTDIR=${destdir} -w -C ${builddir}/gdb/gdbserver"
     dryrun "ln -sfnT ${destdir}/${prefix} /tmp/linaro.$$/${tag}"
 
     local abbrev="`echo ${host}_${target} | sed -e 's:none-::' -e 's:unknown-::'`"
