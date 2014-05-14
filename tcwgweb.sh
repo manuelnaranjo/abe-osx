19f6f16cd56474034ccd78b0267853844d9872e2
Rob Savoye <rob.savoye@linaro.org>
2014-05-14 11:49:11 -0600

    toplevel.txt file renamed to make.log
    Change-Id: Ic1489a141fbd10edcfe19ca62bc288ae1a49a2d4

--- tcwgweb.sh
+++ tcwgweb.sh
@@ -100,7 +100,7 @@ difftwodirs ()
 	    echo "Revision ${cversion} Summary:" >> ${diffdir}/$i-test-results.txt
 	    grep "^# of " ${next}/$i.sum >> ${diffdir}/$i-test-results.txt
 	    echo "" >> ${diffdir}/$i-test-results.txt
-	    echo "Build log: http://cbuild.validation.linaro.org/build/${next}/toplevel.txt" >> ${diffdir}/$i-test-results.txt
+	    echo "Build log: http://cbuild.validation.linaro.org/build/${next}/make.log" >> ${diffdir}/$i-test-results.txt
 	    echo "Test log: http://cbuild.validation.linaro.org/build/${next}/gcc.sum.xz" >> ${diffdir}/$i-test-results.txt
 	    local userid="`grep 'email=' ${next}/manifest.txt | cut -d '=' -f 2`"
 	    if test -e ${diffdir}/$i-test-results.txt; then
