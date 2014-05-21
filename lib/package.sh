155decc68d8283ba78659f7516c05eabf35a6881
Rob Savoye <rob.savoye@linaro.org>
2014-05-21 12:18:51 -0600

    use -static instead of --all-static for building binary packages
    Change-Id: I9da38804e33d9ba6eb8281526555a70c9605fd58

--- lib/package.sh
+++ lib/package.sh
@@ -252,7 +252,7 @@ binary_toolchain()
 
     local builddir="`get_builddir ${binutils_version}`"
     if test x"${binutils_static}" = x"yes"; then
-	local make_flags="${make_flags} LDFLAGS=-all-static  LDFLAGS_FOR_BUILD=-all-static"
+	local make_flags="${make_flags} LDFLAGS=-all-static  LDFLAGS_FOR_BUILD=-static"
         # Binutils executables we want to relink
      	local bins="bfd/doc/chew gold/ld-new gold/incremental-dump gold/dwp binutils/ranlib binutils/objdump binutils/readelf binutils/nm-new binutils/bfdtest1 binutils/size binutils/cxxfilt binutils/addr2line binutils/elfedit binutils/ar binutils/strings binutils/bfdtest2 binutils/strip-new binutils/sysinfo binutils/objcopy ld/ld-new gas/as-new" # gprof/gprof
      	dryrun "(cd ${builddir} && rm -f ${bins})"
