f561469921a8e6952c55d140853d22c74dd84de2
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2014-06-06 00:47:32 +0100

    Parallelize schroot generation on TCWG build farm
    Change-Id: I012bac4c3a2a53b21ad1d9b5ff5699a7d286f9be

--- config/schroot-master/tcwg-schroot-farm.sh
+++ config/schroot-master/tcwg-schroot-farm.sh
@@ -6,18 +6,40 @@ set -x
 master="$(dirname $0)"
 test_schroot="$master/../../scripts/test-schroot.sh"
 
-# Generate chroots for all supported targets on tcwgbuild01, and then copy them to all other tcwgbuildXX machines.
-gen="-g"
-for m in $(for i in `seq 1 6`; do echo tcwgbuild0$i; done); do
-    for a in aarch64-linux-gnu arm-linux-gnueabi arm-linux-gnueabihf i686-linux-gnu mips-linux-gnu mipsel-linux-gnu powerpc-linux-gnu x86_64-linux-gnu; do
-	$test_schroot -v -c $master $gen -a $a $m
+tcwgbuild_archs=(
+    aarch64-linux-gnu
+    arm-linux-gnueabi
+    arm-linux-gnueabihf
+    i686-linux-gnu
+    mips-linux-gnu
+    mipsel-linux-gnu
+    powerpc-linux-gnu
+    x86_64-linux-gnu
+)
+
+# Generate chroots for all supported targets on tcwgbuild01 ...
+pids=""
+for a in "${tcwgbuild_archs[@]}"; do
+    eval $test_schroot -v -g -c $master -a $a tcwgbuild01 &
+    pids="$pids $!"
+done
+wait $pids
+
+# ... then copy them to all other tcwgbuildXX machines.
+pids=""
+for m in $(for i in `seq 2 6`; do echo tcwgbuild0$i; done); do
+    for a in "${tcwgbuild_archs[@]}"; do
+	eval $test_schroot -c $master -a $a $m &
+	pids="$pids $!"
     done
-    gen=""
+done
+# Copy ARMv7 hard-fp and soft-fp chroots to all chromebooks and some blacks.
+for m in $(for i in `seq 1 8`; do echo linaro@tcwgchromebook0$i; done) $(for i in `seq 1 6`; do echo tcwgblack0$i; done); do
+    for a in arm-linux-gnueabi arm-linux-gnueabihf; do
+	eval $test_schroot -c $master -a $a $m &
+	pids="$pids $!"
+    done
 done
 
-# Copy ARMv7 hard-fp and soft-fp chroots to all chromebooks and some blacks.
-for m in $(for i in `seq 1 8`; do echo tcwgchromebook0$i tcwgblack0$i; done); do
-    for a in arm-linux-gnueabi arm-linux-gnueabihf; do
-	$test_schroot -v -c $master $gen -a $a $m
-    done
-done
+wait $pids
+echo ALL DONE
