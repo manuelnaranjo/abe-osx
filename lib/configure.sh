34404c2e605346e5e916924078cf72bf7bec4f78
Rob Savoye <rob.savoye@linaro.org>
2014-06-19 16:05:00 -0600

    make the opts variable local to avoid contamination between stages.

--- lib/configure.sh
+++ lib/configure.sh
@@ -53,11 +53,11 @@ configure_build()
     if test ! -d "${builddir}"; then
 	notice "The build directory '${builddir}' doesn't exist, so creating it"
 	# Zlib has to be built in it's source directory.
-	if test x"${tool}" = x"zlib"; then
-	    dryrun "ln -s ${srcdir} ${builddir}"
-	else
+#	if test x"${tool}" = x"zlib"; then
+#	    dryrun "ln -s ${srcdir} ${builddir}"
+#	else
 	    mkdir -p ${builddir}
-	fi
+#	fi
     fi
 
     if test ! -f "${srcdir}/configure" -a x"${dryrun}" != x"yes"; then
@@ -83,10 +83,10 @@ configure_build()
 #    fi
 
     # Load the default config file for this component if it exists.
-    default_configure_flags=""
-    stage1_flags=""
-    stage2_flags=""
-    opts=""
+    local default_configure_flags=""
+    local stage1_flags=""
+    local stage2_flags=""
+    local opts=""
     if test x"$2" = x"gdbserver"; then
 	local toolname="gdbserver"
     else
@@ -119,18 +119,18 @@ configure_build()
 	for i in "${depends}"; do
 	    # remove the current build component from the command line arguments
 	    # so we can replace it with the dependent component name.
-	    args="`echo ${command_line_arguments} | sed -e 's@$1@@'`"
+	    local args="`echo ${command_line_arguments} | sed -e 's@$1@@'`"
 	done
     fi
 
     # Force static linking unless dynamic linking is specified
-    if test x"${static_link}" != x"no"; then
-	opts="--disable-shared --enable-static"
+    if test x"${static_link}" != x"no" -a x"${tool}" != x"gcc"; then
+	local opts="--disable-shared --enable-static"
     fi
 
     # prefix is the root everything gets installed under.
     if test x"${prefix}" = x; then
-	prefix="${local_builds}/destdir/${host}"
+	local prefix="${local_builds}/destdir/${host}"
     fi
 
     # The release strig is usually the date as well, but in YYYY.MM format.
@@ -144,15 +144,15 @@ configure_build()
     # GCC and the binutils are the only toolchain components that need the
     # --target option set, as they generate code for the target, not the host.
     case ${tool} in
-	zlib)
-	    # zlib doesn't support most standard configure options
-	    opts="--prefix=${sysroots}/usr"
-	    ;;
+	# zlib)
+	#     # zlib doesn't support most standard configure options
+	#     local opts="--prefix=${sysroots}/usr"
+	#     ;;
 	newlib*|libgloss*)
-	    opts="${opts} --build=${build} --host=${target} --target=${target} --prefix=${sysroots}/usr CC=${target}-gcc"
+	    local opts="${opts} --build=${build} --host=${target} --target=${target} --prefix=${sysroots}/usr CC=${target}-gcc"
 	    ;;
 	*libc)
-	    opts="${opts} --build=${build} --host=${target} --target=${target} --prefix=/usr"
+	    local opts="${opts} --build=${build} --host=${target} --target=${target} --prefix=/usr"
 	    dryrun "(mkdir -p ${sysroots}/usr/lib)"
 	    ;;
 	gcc*)
@@ -168,12 +168,12 @@ configure_build()
 		    case $2 in
 			stage1*)
 			    notice "Building stage 1 of GCC"
-			    opts="${opts} ${stage1_flags}"
+			    local opts="${opts} ${stage1_flags}"
 			    ;;
 			stage2*)
 			    notice "Building stage 2 of GCC"
-			    opts="${opts} ${stage2_flags}"
- 			    opts="${opts} --with-bugurl=\"https://bugs.launchpad.net/gcc-linaro\" --with-pkgversion=\"Linaro GCC ${date}\""
+			    local opts="${opts} ${stage2_flags}"
+ 			    local opts="${opts} --with-bugurl=\"https://bugs.launchpad.net/gcc-linaro\" --with-pkgversion=\"Linaro GCC ${date}\""
 			    ;;
 			gdbserver)
 			    notice "Building gdbserver for the target"
@@ -181,55 +181,54 @@ configure_build()
 			    ;;
 			bootstrap*)
 			    notice "Building bootstrapped GCC"
-			    opts="${opts} --enable-bootstrap"
+			    local opts="${opts} --enable-bootstrap"
 			    ;;
 			*)
 			    if test -e ${sysroots}/usr/include/stdio.h; then
 				notice "Building with stage 2 flags, sysroot found!"
-				opts="${opts} ${stage2_flags}"
+				local opts="${opts} ${stage2_flags}"
 			    else
 				warning "Building with stage 1 flags, no sysroot found"
-				opts="${opts} ${stage1_flags}"
+				local opts="${opts} ${stage1_flags}"
 			    fi
 			    ;;
 		    esac
 		else
 		    if test -e ${sysroots}/usr/include/stdio.h; then
 			notice "Building with stage 2 flags, sysroot found!"
-			opts="${opts} ${stage2_flags}"
+			local opts="${opts} ${stage2_flags}"
 		    else
 			warning "Building with stage 1 flags, no sysroot found"
-			opts="${opts} ${stage1_flags}"
+			local opts="${opts} ${stage1_flags}"
 		    fi
 		fi
 	    else
-		opts="${opts} ${stage2_flags}"
+		local opts="${opts} ${stage2_flags}"
 	    fi
-	    version="`echo $1 | sed -e 's#[a-zA-Z\+/:@.]*-##' -e 's:\.tar.*::'`"
-	    opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${prefix}"
+	    local version="`echo $1 | sed -e 's#[a-zA-Z\+/:@.]*-##' -e 's:\.tar.*::'`"
+	    local opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${prefix}"
 	    ;;
 	binutils)
-	    opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${prefix}"
+	    local opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${prefix}"
 	    ;;
 	gdb*)
- 	    opts="${opts} --with-bugurl=\"https://bugs.launchpad.net/gcc-linaro\" --with-pkgversion=\"Linaro GDB ${date}\""
+ 	    local opts="${opts} --with-bugurl=\"https://bugs.launchpad.net/gcc-linaro\" --with-pkgversion=\"Linaro GDB ${date}\""
 	    if test x"$2" = x"gdbserver"; then
-		opts="${opts} --build=${build} --host=${target} --prefix=${prefix}"
+		local opts="${opts} --build=${build} --host=${target} --prefix=${prefix}"
 		local srcdir="${srcdir}/gdb/gdbserver"
 	    else
-		opts="${opts} --build=${build} --host=${host} --prefix=${prefix}"
+		local opts="${opts} --build=${build} --host=${host} --prefix=${prefix}"
 	    fi
 	    dryrun "mkdir -p ${builddir}"
 	    ;;
 	# These are only built for the host
 	dejagnu|gmp|mpc|mpfr|isl|ppl|cloog|qt-everywhere-opensource-src|ffmpeg)
-	    opts="${opts} --build=${build} --host=${host} --prefix=${prefix}"
+	    local opts="${opts} --build=${build} --host=${host} --prefix=${prefix}"
 	    ;;
 	*)
-	    opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${sysroots}/usr"
+	    local opts="${opts} --build=${build} --host=${host} --target=${target} --prefix=${sysroots}/usr"
 	    ;;
     esac
-
 
     if test -e ${builddir}/config.status -a x"${tool}" != x"gcc" -a x"${force}" = xno; then
 	warning "${buildir} already configured!"
@@ -250,17 +249,20 @@ configure_build()
 
 	# zlib can only be configured in the source tree, and doesn't like any of the
 	# stadard GNU configure options
-	if test x"${tool}" = x"zlib"; then
-	    dryrun "(cd ${builddir} && ${CONFIG_SHELL} ./configure --prefix=${prefix})"
-	else
+#	if test x"${tool}" = x"zlib"; then
+#	    dryrun "(cd ${builddir} && ${CONFIG_SHELL} ./configure --prefix=${prefix})"
+#	else
 	    dryrun "(cd ${builddir} && ${CONFIG_SHELL} ${srcdir}/configure ${default_configure_flags} ${opts})"
-	fi
+#	fi
 	if test $? -gt 0; then
 	    error "Configure of $1 failed."
 	    return $?
 	fi
 	# unset this to avoid problems later
-	default_configure_flags=
+	unset default_configure_flags=
+	unset opts
+	unset stage1_flags
+	unset stage2_flags
     fi
 
     notice "Done configuring ${gitinfo}"
