bea5f0b16680ef3a3fa46c54ffa8e9073a578b3a
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2014-09-30 07:32:08 +0100

    Revert "Follow up previous typo with this one"
    This reverts commit e71ae198379202fd4f5178c025c9806dd9de777b.

--- scripts/test-schroot.sh
+++ scripts/test-schroot.sh
@@ -43,7 +43,7 @@ triplet_to_deb_arch()
     set -e
     case "$1" in
 	aarch64-*linux-gnu) echo arm64 ;;
-	arrm-*linux-gnueabi*) echo armhf ;;
+	arm-*linux-gnueabi*) echo armhf ;;
 	i686-*linux-gnu) echo i386 ;;
 	x86_64-*linux-gnu) echo amd64 ;;
 	*) return 1 ;;
