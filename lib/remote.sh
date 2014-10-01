085c4602f33adb5c9f3f37b97625a3d510189a42
Bernard Ogden <bernie.ogden@linaro.org>
2014-10-01 13:59:40 +0100

    Fix substitutions in remote_exec*
    Wrote an epic comment giving examples of working commands.
    
    Haven't fixed up to use "" around user input everywhere, just where
    it wouldn't be a wild corner case.
    
    Change-Id: I6d6d9e45be80db2c0ac73a383f619be6f38812e9

--- lib/remote.sh
+++ lib/remote.sh
@@ -1,6 +1,45 @@
 #TODO: Check that remote_{down,up)load behave as one might think
 #TODO: Copy more of the dejagnu contract? e.g. return target-side name of copied file
 #TODO: Tests?
+
+#ON USING remote_exec*
+#---------------------
+#Quotes in command can be tricky. It's a good idea to use single-quotes when
+#you can get away with it, because these are easier to think about. Remember:
+#1) It isn't legal to escape a single-quote within single-quotes
+#2) \ is only removed on expansion if it actualy escaped something
+#3) \ only escapes $,`,\ when in `` (critically, it does not escape either kind of quote) - but if you push \ through to the execution of the contents of the `` (by typing \\ everywhere you want \) then will of course behave as normal when shoved through the shell commands within  the ``.
+#Because of (1), it's sensible to use "" rather than '' when we're expecting input that
+#may in turn contain quotes. And actually more robust to do that in general.
+#I may never fathom the laws on behaviour of \, so further dragons may lurk here.
+#Some examples of how to call:
+#remote_exec2 localhost "grep 'foo bar' ~/file"
+##Becomes ssh localhost "grep 'foo bar' ~/file"
+##Becomes grep 'foo bar' ~/file
+#
+#x="`remote_exec2 localhost \"grep 'foo bar' ~/file\"`"
+##Becomes ssh localhost "grep 'foo bar' ~/file"
+##Becomes grep 'foo bar' ~/file
+##So x ends up as the non-expanded, non-globbed output of that grep command
+#
+#Four ways to grep for a single-quote - the first 2 are vim-highlighting-friendly
+#x="$(remote_exec2 localhost "grep \"'\" ~/file")"
+#x="`remote_exec2 localhost "grep \\\"'\\\" ~/file"`"
+#x="`remote_exec2 localhost \"grep \\\"'\\\" ~/file\"`"
+#x="`remote_exec2 localhost "grep \\"'\\" ~/file"`"
+#These (all?) effectively expand to:
+##ssh localhost "grep \"'\" ~/file"
+##grep "'" ~/file
+##So x ends up as the non-expanded, non-globbed output of that grep command
+#
+#One way to grep for a double-quote.
+#x="`remote_exec localhost "grep \\\"\\\\\\\\\\"\\\" ~/file"`"
+#My personal favourite. Using single-quotes would be much simpler, but there
+#are real cases where we can't do that. Expands to:
+##ssh localhost "grep \"\\\"\" ~/file"
+##grep "\"" ~/file
+##So x ends up as the non-expanded, non-globbed output of that grep command
+
 remote_upload()
 {
   local target="$1"
@@ -30,20 +69,20 @@ remote_upload()
 
 remote_exec()
 {
-  local target="$1"
-  local cmd="$2"
+  local target="${1//\"/\\\"}"
+  local cmd="${2//\"/\\\"}"
   if test ${#@} -lt 2; then
     error "Target and/or command not specified"
     return 1
   fi
-  dryrun "ssh -o PasswordAuthentication=no -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR '${target}' '${cmd}'"
+  dryrun "ssh -o PasswordAuthentication=no -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR \"${target}\" \"${cmd}\""
   return $?
 }
 
 remote_exec_async()
 {
-  local target="$1"
-  local cmd="$2"
+  local target="${1//\"/\\\"}"
+  local cmd="${2//\"/\\\"}"
   local stdoutfile="${3:-stdout}"
   local stderrfile="${4:-stderr}"
   if test ${#@} -lt 2; then
@@ -66,7 +105,7 @@ remote_exec_async()
   #Logging command that docs say is needed to work
   #dryrun "ssh -n -o PasswordAuthentication=no -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR ${target} -- \"nohup bash -c 'exec 1>${stdoutfile}; exec 2>${stderrfile}; ${cmd}; echo EXIT CODE: \$? | tee /dev/console' &\""
 
-  #Using command that experiments show is sufficient (it's a bit less complicated)
-  dryrun "ssh -n -o PasswordAuthentication=no -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR ${target} -- 'exec 1>${stdoutfile}; exec 2>${stderrfile}; ${cmd}; echo EXIT CODE: \$? | tee /dev/console' &"
+  #Using command that experiments show is sufficient
+  dryrun "ssh -n -o PasswordAuthentication=no -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR ${target} -- \"exec 1>${stdoutfile}; exec 2>${stderrfile}; ${cmd}; echo EXIT CODE: \$? | tee /dev/console\" &"
   return $?
 }
