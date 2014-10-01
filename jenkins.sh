bc39dc93aa191e61cffaa365e81d0d3bd3695bdc
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2014-10-01 13:13:19 +0100

    Revert "Slow down testing so that tcwg-chrome2-01 can keep up"
    This reverts commit a2d3d2613c402bae1fa21a2f131e44e0641b45db.

--- jenkins.sh
+++ jenkins.sh
@@ -176,9 +176,7 @@ $CONFIG_SHELL ${cbuild_dir}/configure --with-local-snapshots=${user_snapshots} -
 
 # Double parallelism for tcwg-ex40-* machines to compensate for really-remote
 # target execution.  GCC testsuites will run with -j 32.
-case "$(hostname):${target}" in
-    # !!! Temporarily reduce to 4 so that tcwg-chrome2-01 doesn't crash.
-    "tcwg-ex40-"*":arm-linux-gnueabi"*) sed -i -e "s/cpus=8/cpus=4/" host.conf ;;
+case "$(hostname)" in
     "tcwg-ex40-"*) sed -i -e "s/cpus=8/cpus=16/" host.conf ;;
 esac
 
