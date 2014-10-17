1fb2516228d5e84672fad11791d04473d168fb35
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2014-10-17 08:37:01 +0100

    Update session keep/kill marker file
    Change-Id: I7ad8281cba0c350e7d08e8ebc70029a2b48ffe0b

--- scripts/test-schroot.sh
+++ scripts/test-schroot.sh
@@ -255,6 +255,9 @@ if $begin_session; then
     $rsh root@$target rsync -a /root/ $home/
     $rsh root@$target chown -R $user $home/
 
+    $rsh $target touch /dont_keep_session
+    $rsh $target chmod 0666 /dont_keep_session
+
     echo $target:$port started schroot: $rsh $target
 fi
 
@@ -317,7 +320,7 @@ if $ssh_master; then
 fi
 
 # Keep the session alive when file /dont_kill_me is present
-if $finish_session && `$schroot test ! -f "/dont_kill_me"`; then
+if $finish_session && `$schroot test -f /dont_keep_session`; then
     $schroot iptables -I INPUT -p tcp --dport $port -j REJECT || true
     ssh $target_ssh_opts $target schroot -f -e -c session:tcwg-test-$port | true
     if [ x"${PIPESTATUS[0]}" != x"0" ]; then
