bbfdf8a6286c7242bc82dc2320fab9c303980a27
Rob Savoye <rob.savoye@linaro.org>
2015-01-23 21:08:44 -0700

    diff two test result files

--- lib/report.sh
+++ lib/report.sh
@@ -105,8 +105,103 @@ extract_results ()
     return 0
 }
 
-# FIXME: for now this is hardcoded, but wil lbe passed in by Jenkins
-toplevel="/work/logs/gcc-linaro/4.9-backport-218451-2/Backport32"
+# Process and diff two sum files.
+dodiff ()
+{
+    sort $1 -o ${toplevel}/head-sort.sum
+    sort $2 -o ${toplevel}/head-1-sort.sum
+
+    diff -U 0 ${toplevel}/head-sort.sum ${toplevel}/head-1-sort.sum 2>&1 | egrep '^[+-]PASS|^[+-]FAIL|^[+-]XPASS|^[+-]XFAIL|^[+-]UNRESOLVED|^[+-]UNSUPPORTED|^[+-]UNTESTED' 2>&1 | sort -k 2 2>&1 > ${toplevel}/diff.txt
+    
+    if test -s ${toplevel}/diff.txt; then
+	declare -a diff=()
+	local i=0
+	while read line
+	do
+	    diff[$i]="$line"
+	    i="`expr $i + 1`"
+	done < ${toplevel}/diff.txt
+
+	local i=0
+	local j=0
+	declare -A status
+	while test $j -lt ${#diff[@]}; do
+	    j="`expr $i + 1`"
+	    local str1="`echo ${diff[$i]} | cut -d ' ' -f 2-30`"
+	    local str2="`echo ${diff[$j]} | cut -d ' ' -f 2-30`"
+	    local sign[0]=`expr substr "${diff[$i]}" 1 1`
+	    local sign[1]=`expr substr "${diff[$j]}" 1 1`
+#	    echo "FIXIT: ${diff[$i]} ${diff[$j]}"
+	    if test x"${str1}" = x"${str2}" -a x"${str1}" != x; then
+#		echo "FIXME: regression in!!! ${str1}"
+		case "${diff[$i]} ${diff[$j]}" in
+		    -FAIL:*PASS:*)
+#			echo "FIXME: FAIL->PASS"
+			;;
+		    -PASS:*+XFAIL:*)
+#			echo "FIXME: PASS->XFAIL"
+			;;
+		    -PASS:*+FAIL:*)
+			status[PASSNOWFAILS]="${str1}"
+#			echo "FIXME: PASS->FAIL"
+			;;
+		    +XPASS:*-FAIL:*) 
+#			echo "FIXME: XPASS->FAIL"
+			;;
+		    +XFAIL:*-FAIL:*)
+#			echo "FIXME: XFAIL->FAIL"
+			;;
+		    +XFAIL:*-PASS:*) 
+#			echo "FIXME: XFAIL->PASS"
+			;;
+		    -PASS:*XPASS:*)
+#			echo "FIXME: PASS=>XPASS"
+			;;
+		    +FAIL:*-PASS:*)
+			status[FAILNOWPASS]="${str1}"
+#			echo "FIXME: FAIL->PASS"
+			;;
+		    *) echo
+			"FIXEEE: ${foo}"
+			;;
+		esac
+#		local state="`expr substr "${diff[$i]}" 1 5`"
+		i="`expr $i + 2`"
+	    else
+		if test x"${sign[0]}" = x -a x"${sign[1]}" = x; then
+		    break
+		fi
+#		echo "FIXME: New or deleted test!!!!"
+		if test x"${sign[0]}" = x"-"; then
+#		    echo "FIXME: ${str1} was removed"
+		    status[DISAPPEARED]="${str1}"
+		else
+		    if test x"${sign[1]}" = x"+"; then
+#			echo "FIXME: ${str2} was added"			
+			status[APPEARS]="${str1}"
+		    fi
+		fi
+		i="`expr $i + 1`"
+	    fi
+	done
+
+	# This obscure option to declare dumps the array as an associative array
+	declare -p status
+	return 0
+    fi
+    
+    return 1
+}
+
+if test x"${1}" != x; then
+    toplevel="$1"
+else
+    # FIXME: for now this is hardcoded, but will be passed in by Jenkins
+    toplevel="/work/logs/gcc-linaro/4.9-backport-218451-2/Backport32"
+fi
+
+builds="`find ${toplevel} -type d`"
+
 sums="`find ${toplevel} -name gcc.sum*`"
 declare -a head=()
 i=0
@@ -183,11 +278,291 @@ while test $i -lt ${#head[@]}; do
     i="`expr $i + 1`"
 done
 
+i=0
+
+eval "`dodiff ${sums[0]} ${sums[1]}`"
+if test "$?" -eq 0; then
+    echo "No regressions"
+fi
+
+if test x"${1}" != x; then
+    toplevel="$1"
+else
+    # FIXME: for now this is hardcoded, but will be passed in by Jenkins
+    toplevel="/work/logs/gcc-linaro/4.9-backport-218451-2/Backport32"
+fi
+
+builds="`find ${toplevel} -type d`"
+
+sums="`find ${toplevel} -name gcc.sum*`"
+declare -a head=()
+i=0
+for sum in ${sums}; do
+    if test `echo ${sum} | grep -c "\.xz$"` -gt 0; then
+	unxz ${sum}
+    fi
+    file="`echo ${sum} | sed -e 's:\.xz::'`"
+    head[$i]="`extract_results ${file}`"
+    i="`expr $i + 1`"
+done
+
+declare -A totals=()
+totals[PASSES]=0
+totals[XPASSES]=0
+totals[FAILURES]=0
+totals[XFAILURES]=0
+totals[UNRESOLVED]=0
+totals[UNSUPPORTED]=0
+i=0
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    totals[PASSES]="`expr ${totals[PASSES]} + ${data[PASSES]:-0}`"
+    totals[XPASSES]="`expr ${totals[XPASSES]} + ${data[XPASSES]:-0}`"
+    totals[FAILURES]="`expr ${totals[FAILURES]} + ${data[FAILURES]:-0}`"
+    totals[XFAILURES]="`expr ${totals[XFAILURES]} + ${data[XFAILURES]:-0}`"
+    totals[UNRESOLVED]="`expr ${totals[UNRESOLVED]} + ${data[UNRESOLVED]:-0}`"
+    totals[UNSUPPORTED]="`expr ${totals[UNSUPPORTED]} + ${data[UNSUPPORTED]:-0}`"
+    i="`expr $i + 1`"
+done
+
+# Status messages
+declare ok_msgs=(\
+    "Still passes              [PASS => PASS]" \
+    "Still fails               [FAIL => FAIL]")
+
+declare checked_msgs=(\
+    "Xfail appears             [PASS =>XFAIL]" \
+    "Timeout                   [PASS =>T.OUT]" \
+    "Fail disappears           [FAIL =>     ]" \
+    "Expected fail passes      [XFAIL=>XPASS]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "New pass                  [     => PASS]" \
+    "Unhandled cases           [   ..??..   ]" \
+    "Unstable cases            [PASS => FAIL]")
+
+declare error_msgs=(
+    "Passed now fails          [PASS => FAIL]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "Pass disappears           [PASS =>     ]" \
+    "Fail appears              [     => FAIL]" \
+    "Timeout                   [PASS =>T.OUT]")
+
+# Run status categories and totals
+declare categories=(\
+    "Passes                      [PASS+XPASS]" \
+    "Unexpected fails                  [FAIL]" \
+    "Expected fails                   [XFAIL]" \
+    "Unresolved                  [UNRESOLVED]" \
+    "Unsupported       [UNTESTED+UNSUPPORTED]")
+
+
+declare -a dir=()
+i=0
+#j=1
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    tmp="`dirname ${data[FILESPEC]}`"
+   if test x"${tmp}" != dir[$i]; then
+	dir[$i]="`dirname ${tmp}`"
+	echo "DIR: ${dir[$i]}"
+    fi
+#    j="`expr $j - 1`"
+    i="`expr $i + 1`"
+done
+
+i=0
+
+eval "`dodiff ${sums[0]} ${sums[1]}`"
+if test "$?" -eq 0; then
+    echo "No regressions"
+fi
+
+if test x"${1}" != x; then
+    toplevel="$1"
+else
+    # FIXME: for now this is hardcoded, but will be passed in by Jenkins
+    toplevel="/work/logs/gcc-linaro/4.9-backport-218451-2/Backport32"
+fi
+
+builds="`find ${toplevel} -type d`"
+
+sums="`find ${toplevel} -name gcc.sum*`"
+declare -a head=()
+i=0
+for sum in ${sums}; do
+    if test `echo ${sum} | grep -c "\.xz$"` -gt 0; then
+	unxz ${sum}
+    fi
+    file="`echo ${sum} | sed -e 's:\.xz::'`"
+    head[$i]="`extract_results ${file}`"
+    i="`expr $i + 1`"
+done
+
+declare -A totals=()
+totals[PASSES]=0
+totals[XPASSES]=0
+totals[FAILURES]=0
+totals[XFAILURES]=0
+totals[UNRESOLVED]=0
+totals[UNSUPPORTED]=0
+i=0
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    totals[PASSES]="`expr ${totals[PASSES]} + ${data[PASSES]:-0}`"
+    totals[XPASSES]="`expr ${totals[XPASSES]} + ${data[XPASSES]:-0}`"
+    totals[FAILURES]="`expr ${totals[FAILURES]} + ${data[FAILURES]:-0}`"
+    totals[XFAILURES]="`expr ${totals[XFAILURES]} + ${data[XFAILURES]:-0}`"
+    totals[UNRESOLVED]="`expr ${totals[UNRESOLVED]} + ${data[UNRESOLVED]:-0}`"
+    totals[UNSUPPORTED]="`expr ${totals[UNSUPPORTED]} + ${data[UNSUPPORTED]:-0}`"
+    i="`expr $i + 1`"
+done
+
+# Status messages
+declare ok_msgs=(\
+    "Still passes              [PASS => PASS]" \
+    "Still fails               [FAIL => FAIL]")
+
+declare checked_msgs=(\
+    "Xfail appears             [PASS =>XFAIL]" \
+    "Timeout                   [PASS =>T.OUT]" \
+    "Fail disappears           [FAIL =>     ]" \
+    "Expected fail passes      [XFAIL=>XPASS]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "New pass                  [     => PASS]" \
+    "Unhandled cases           [   ..??..   ]" \
+    "Unstable cases            [PASS => FAIL]")
+
+declare error_msgs=(
+    "Passed now fails          [PASS => FAIL]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "Pass disappears           [PASS =>     ]" \
+    "Fail appears              [     => FAIL]" \
+    "Timeout                   [PASS =>T.OUT]")
+
+# Run status categories and totals
+declare categories=(\
+    "Passes                      [PASS+XPASS]" \
+    "Unexpected fails                  [FAIL]" \
+    "Expected fails                   [XFAIL]" \
+    "Unresolved                  [UNRESOLVED]" \
+    "Unsupported       [UNTESTED+UNSUPPORTED]")
+
+
+declare -a dir=()
+i=0
+#j=1
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    tmp="`dirname ${data[FILESPEC]}`"
+   if test x"${tmp}" != dir[$i]; then
+	dir[$i]="`dirname ${tmp}`"
+	echo "DIR: ${dir[$i]}"
+    fi
+#    j="`expr $j - 1`"
+    i="`expr $i + 1`"
+done
+
+i=0
+
+if test x"${1}" != x; then
+    toplevel="$1"
+else
+    # FIXME: for now this is hardcoded, but will be passed in by Jenkins
+    toplevel="/work/logs/gcc-linaro/4.9-backport-218451-2/Backport32"
+fi
+
+builds="`find ${toplevel} -type d`"
+
+sums="`find ${toplevel} -name gcc.sum*`"
+declare -a head=()
+i=0
+for sum in ${sums}; do
+    if test `echo ${sum} | grep -c "\.xz$"` -gt 0; then
+	unxz ${sum}
+    fi
+    file="`echo ${sum} | sed -e 's:\.xz::'`"
+    head[$i]="`extract_results ${file}`"
+    i="`expr $i + 1`"
+done
+
+declare -A totals=()
+totals[PASSES]=0
+totals[XPASSES]=0
+totals[FAILURES]=0
+totals[XFAILURES]=0
+totals[UNRESOLVED]=0
+totals[UNSUPPORTED]=0
+i=0
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    totals[PASSES]="`expr ${totals[PASSES]} + ${data[PASSES]:-0}`"
+    totals[XPASSES]="`expr ${totals[XPASSES]} + ${data[XPASSES]:-0}`"
+    totals[FAILURES]="`expr ${totals[FAILURES]} + ${data[FAILURES]:-0}`"
+    totals[XFAILURES]="`expr ${totals[XFAILURES]} + ${data[XFAILURES]:-0}`"
+    totals[UNRESOLVED]="`expr ${totals[UNRESOLVED]} + ${data[UNRESOLVED]:-0}`"
+    totals[UNSUPPORTED]="`expr ${totals[UNSUPPORTED]} + ${data[UNSUPPORTED]:-0}`"
+    i="`expr $i + 1`"
+done
+
+# Status messages
+declare ok_msgs=(\
+    "Still passes              [PASS => PASS]" \
+    "Still fails               [FAIL => FAIL]")
+
+declare checked_msgs=(\
+    "Xfail appears             [PASS =>XFAIL]" \
+    "Timeout                   [PASS =>T.OUT]" \
+    "Fail disappears           [FAIL =>     ]" \
+    "Expected fail passes      [XFAIL=>XPASS]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "New pass                  [     => PASS]" \
+    "Unhandled cases           [   ..??..   ]" \
+    "Unstable cases            [PASS => FAIL]")
+
+declare error_msgs=(
+    "Passed now fails          [PASS => FAIL]" \
+    "Fail now passes           [FAIL => PASS]" \
+    "Pass disappears           [PASS =>     ]" \
+    "Fail appears              [     => FAIL]" \
+    "Timeout                   [PASS =>T.OUT]")
+
+# Run status categories and totals
+declare categories=(\
+    "Passes                      [PASS+XPASS]" \
+    "Unexpected fails                  [FAIL]" \
+    "Expected fails                   [XFAIL]" \
+    "Unresolved                  [UNRESOLVED]" \
+    "Unsupported       [UNTESTED+UNSUPPORTED]")
+
+
+declare -a dir=()
+i=0
+#j=1
+while test $i -lt ${#head[@]}; do
+    eval declare -A data=(${head[$i]})
+    tmp="`dirname ${data[FILESPEC]}`"
+   if test x"${tmp}" != dir[$i]; then
+	dir[$i]="`dirname ${tmp}`"
+	echo "DIR: ${dir[$i]}"
+    fi
+#    j="`expr $j - 1`"
+    i="`expr $i + 1`"
+done
+
+i=0
+
+eval "`dodiff ${sums[0]} ${sums[1]}`"
+dodiff ${sums[0]} ${sums[1]}
+if test "$?" -eq 0; then
+    echo "No regressions"
+fi
+
+echo "FIXME TOO: ${status[FAILNOWPASS]}"
 
 i=0
 while test $i -lt ${#head[@]}; do
     eval declare -A data=(${head[$i]})
-    display_header "${data[TARGET]}" "dir1" "dir2" 2
+    display_header "${data[TARGET]}" "${sums[0]}" "${sums[1]}" 2
     declare msgs=("${error_msgs[0]}" "${error_msgs[2]}")
     regression_table "REGRESSIONS" msgs[@] totals[@]
     #regression_table "MINOR TO BE CHECKED" msgs[@] counts[@]
