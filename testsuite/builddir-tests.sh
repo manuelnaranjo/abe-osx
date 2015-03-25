1ec00f40c0b6c537a739d2ee24f18922d9f8f81d
Rob Savoye <rob.savoye@linaro.org>
2015-03-25 10:25:16 -0600

    remove bzr test for get_builddir, as bzr support in ABE is being depreciated.

--- testsuite/builddir-tests.sh
+++ testsuite/builddir-tests.sh
@@ -29,15 +29,6 @@ else
     fixme "${in} returned ${out}"
 fi
 
-in="lp:gdb-linaro/7.5"
-out="`get_builddir ${in}`"
-if test ${out} = "${local_builds}/x86_64-unknown-linux-gnu/x86_64-linux-gnu/gdb-linaro_7.5"; then
-    pass "get_builddir: bzr branch"
-else
-    fail "get_builddir: bzr branch"
-    fixme "${in} returned ${out}"
-fi
-
 in="git://git.linaro.org/toolchain/binutils.git"
 out="`get_builddir ${in}~binutils-2_18-branch@654321`"
 match="${local_builds}/${build}/x86_64-linux-gnu/binutils.git~binutils-2_18-branch@654321"
