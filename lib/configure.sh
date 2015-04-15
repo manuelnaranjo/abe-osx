2d48b697202d82af5a99c888e4dd208b5778d6bf
Rob Savoye <rob.savoye@linaro.org>
2015-04-15 16:04:38 -0600

    remove debug FOO

--- lib/configure.sh
+++ lib/configure.sh
@@ -68,7 +68,6 @@ configure_build()
     # Since Binutils and GDB have a shared git repository, this confuses the logic
     # of loading the right config file. If Binutils is already configured, then 
     # assume GDB.
-    echo "FOO"
     if test x"${2}" = x"gdb" -o x"${2}" = x"binutils"; then
 	local tool="${2}"
     fi
