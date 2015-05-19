manuelnaranjo/abe-osx

commit 691a304d4f074bcee8acde4e6c7adee628bf8ea8

change wget to use fileserver and remote_snapshots variables

lib/fetch.sh
@@ -142,7 +142,7 @@ fetch_http()
 	    # NOTE: the timeout is short, and we only try twice to access the
 	    # remote host. This is to improve performance when offline, or
 	    # the remote host is offline.
-	    dryrun "${wget_bin} ${wget_quiet:+-q} --timeout=${wget_timeout}${wget_progress_style:+ --progress=${wget_progress_style}} --tries=2 --directory-prefix=${local_snapshots}/${dir} ${remote_snapshots}/${getfile}"
+	    dryrun "${wget_bin} ${wget_quiet:+-q} --timeout=${wget_timeout}${wget_progress_style:+ --progress=${wget_progress_style}} --tries=2 --directory-prefix=${local_snapshots}/${dir} http://${fileserver}:${remote_snapshots}/${getfile}"
 	    if test x"${dryrun}" != xyes -a ! -s ${local_snapshots}/${getfile}; then
 		warning "downloaded file ${getfile} has zero data!"
 		return 1

commit 302e2a9b006210db85acd06c4ccf6f44ed52ac8b

Use '/' instead of ':' to designate remote directory when calling wget.
This fixes a bug introduced by git commit
691a304d4f074bcee8acde4e6c7adee628bf8ea8

fetch_http() calls wget with the following syntax:

http://${fileserver}:${remote_snapshots}/${getfile}

The correct syntax for specifying a remote directory is with a '/' and not
with a ':', i.e.,

http://${fileserver}/${remote_snapshots}/${getfile}

This fix is currently untestable in isolation on master, but the fix has
been verified manually.

Change-Id: I7ebaf9d1c7fc6f131bb48284850fc518d284e052

lib/fetch.sh
@@ -152,7 +152,7 @@ fetch_http()
 	    # NOTE: the timeout is short, and we only try twice to access the
 	    # remote host. This is to improve performance when offline, or
 	    # the remote host is offline.
-	    dryrun "${wget_bin} ${wget_quiet:+-q} --timeout=${wget_timeout}${wget_progress_style:+ --progress=${wget_progress_style}} --tries=2 --directory-prefix=${local_snapshots}/${dir} http://${fileserver}:${remote_snapshots}/${getfile}"
+	    dryrun "${wget_bin} ${wget_quiet:+-q} --timeout=${wget_timeout}${wget_progress_style:+ --progress=${wget_progress_style}} --tries=2 --directory-prefix=${local_snapshots}/${dir} http://${fileserver}/${remote_snapshots}/${getfile}"
 	    if test x"${dryrun}" != xyes -a ! -s ${local_snapshots}/${getfile}; then
 		warning "downloaded file ${getfile} has zero data!"
 		return 1