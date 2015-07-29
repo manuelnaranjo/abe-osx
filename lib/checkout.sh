manuelnaranjo/abe-osx

commit 015126dd6cfda2a055d111567b39d14367468b90

don't try to checkout a source tarball from git if it exists

lib/checkout.sh
@@ -148,13 +148,15 @@ checkout_all()
 	esac
 
     	local gitinfo="`get_source ${package}`"
-	local checkout_ret=
-	checkout ${gitinfo}
-	checkout_ret=$?
+	if test x"${gitinfo}" = x; then
+	    local checkout_ret=
+	    checkout ${gitinfo}
+	    checkout_ret=$?
 
-	if test ${checkout_ret} -gt 0; then
-	    error "Failed checkout out of $i."
-	    return 1
+	    if test ${checkout_ret} -gt 0; then
+		error "Failed checkout out of $i."
+		return 1
+	    fi
 	fi
     done
     

commit 4456b29bebefa8d5bd820e0d6e0d951acb522fcb

lib/checkout.sh: Restore "--checkout all" functionality.
Fix bug introduced by git commit 015126dd6cfda2a055d111567b39d14367468b90.

Change-Id: I945a193f3ac00ecd9ae5b73a9e4d0243d2810119

lib/checkout.sh
@@ -184,7 +184,7 @@ checkout_all()
 	esac
 
     	local gitinfo="`get_source ${package}`"
-	if test x"${gitinfo}" = x; then
+	if test x"${gitinfo}" != x; then
 	    local checkout_ret=
 	    checkout ${gitinfo}
 	    checkout_ret=$?
@@ -193,6 +193,9 @@ checkout_all()
 		error "Failed checkout out of $i."
 		return 1
 	    fi
+	else
+	    error "Unable to find source url for package $package."
+	    return 1
 	fi
     done
     