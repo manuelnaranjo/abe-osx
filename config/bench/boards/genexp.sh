8a812eb2e8310641f71e1573a92cc3dcfad7072d
Bernard Ogden <bernie.ogden@linaro.org>
2015-10-02 12:14:05 +0100

    Support all frequencies

--- config/bench/boards/genexp.sh
+++ config/bench/boards/genexp.sh
@@ -4,21 +4,20 @@ set -ue -o pipefail
 
 other=0
 alt_other=5
+size=(L B B L L L)
+declare -A freqL=([slowest]=450MHz [slower]=575MHz [freq]=700MHz [faster]=775MHz [fastest]=850MHz)
+declare -A freqB=([slowest]=450MHz [slower]=625MHz [freq]=800MHz [faster]=950MHz [fastest]=1100MHz)
 for y in 0 1 2 3 4 5; do
-  for x in base freq net aslr env nice; do
+  for x in base slowest slower freq faster fastest net aslr env nice; do
     cat > noise-control-experiment-juno-${y}-${x}.conf << EOF
 #$y $x
 benchcore=$y
 othercore=`if test $y -eq $other; then echo $alt_other; else echo $other; fi`
-freq=`if test $x = freq; then
-         for z in 1 2; do
-           if test $y -eq $z; then
-             echo 800MHz
-             exit
-           fi
-         done
-         echo 700MHz
-       fi`
+freq=`if test x${size[$y]} = xL; then
+        echo ${freqL[$x]:-}
+      elif test x${size[$y]} = xB; then
+        echo ${freqB[$x]:-}
+      fi`
 netctl=`if test $x = net; then echo yes; else echo no; fi`
 aslrctl=`if test $x = aslr; then echo yes; else echo no; fi`
 envctl=`if test $x = env; then echo yes; else echo no; fi`
