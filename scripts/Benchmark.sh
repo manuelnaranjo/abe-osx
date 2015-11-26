2c16e965f6476a428a6d2c256e0d41d8baac4394
Bernard Ogden <bernie.ogden@linaro.org>
2015-11-26 16:11:19 +0000

    Benchmark.sh,template.yaml: Map from target type to image
    Change-Id: I4fc0200ede011f754e7fb88f811a8cc76b3c4285

--- scripts/Benchmark.sh
+++ scripts/Benchmark.sh
@@ -2,10 +2,30 @@
 set -eu
 set -o pipefail
 
+#Mapping from targets to images
+#TODO Two maps to handle some cases having 1 line and some cases having 2 lines
+#     Dreadful hack that will do for now.
+declare -A image_map_1 image_map_2
+image_map_1=(
+[kvm]="image: 'http://images.validation.linaro.org/ubuntu-14-04-server-base.img.gz'"
+[juno]="hwpack: 'http://people.linaro.org/~bernie.ogden/hwpack_linaro-lt-vexpress64-rtsm_20150114-706_arm64_supported.tar.gz'"
+[arndale]="image: 'http://people.linaro.org/~bernie.ogden/arndale/arndale.img'"
+[panda-es]="hwpack: 'http://releases.linaro.org/14.05/ubuntu/panda/hwpack_linaro-panda_20140525-654_armhf_supported.tar.gz'"
+#[mustang]=#tricky, uses a completely different boot method
+)
+image_map_2=(
+[juno]="rootfs: 'http://people.linaro.org/~bernie.ogden/linaro-utopic-developer-20150114-87.tar.gz'"
+[panda-es]="rootfs: 'http://releases.linaro.org/14.05/ubuntu/panda/linaro-trusty-developer-20140522-661.tar.gz'"
+)
+
 if test x"${TARGET_CONFIG%%-*}" = xjuno; then
   TARGET_DEVICE_TYPE=juno
 else
   TARGET_DEVICE_TYPE="${TARGET_CONFIG}"
+fi
+if test x"${image_map_1[${TARGET_DEVICE_TYPE}]}" = x; then
+  echo "No image for target type ${TARGET_DEVICE_TYPE}" >&2
+  exit 1
 fi
 
 #TODO Benchmarking-specific builds will eliminate these special cases
@@ -34,8 +54,8 @@ echo PREBUILT="${PREBUILT:-}"
 echo HOST_SESSION="config/bench/lava/trusted-host-session.yaml"
 echo HOST_IMAGE="http://images.validation.linaro.org/ubuntu-14-04-server-base.img.gz"
 echo TARGET_SESSION="${TARGET_SESSION}"
-#TODO Map from target types to specific images
-echo TARGET_IMAGE="http://images.validation.linaro.org/ubuntu-14-04-server-base.img.gz"
+echo TARGET_IMAGE_1="${image_map_1[${TARGET_DEVICE_TYPE}]}"
+echo TARGET_IMAGE_2="${image_map_2[${TARGET_DEVICE_TYPE}]:-}"
 echo TARGET_CONFIG="${TARGET_CONFIG}"
 echo TARGET_DEVICE_TYPE="${TARGET_DEVICE_TYPE}"
 echo BUNDLE_SERVER="https://${LAVA_SERVER}"
