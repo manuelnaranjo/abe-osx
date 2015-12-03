0e149e94a8f0594e2f644aaf2e9c2401c9b4f60f
Bernard Ogden <bernie.ogden@linaro.org>
2015-12-03 10:30:33 +0000

    Gather script + tests for CPU2006
    Add bc dep to target*.yaml
    
    Change-Id: I990105cd87ec471005d58d148d8355ad01cc668b

---
+++ config/bench/gather/CPU2006.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+error=1
+
+function ltc {
+  #Funky quoting to help out syntax highlighters
+  ${TESTING:+echo} 'lava-test-case' "$@"
+}
+
+function exit_handler {
+  exit ${error}
+}
+
+#This only works in some cases, keeping it as it is useful when it works,
+#not wasting any more time trying to understand it.
+#One fail case is:
+#function ename { name; }
+#ename
+#This exits, but produces no stacktrace.
+function err_handler {
+  exec 1>&2
+  echo "ERROR ${error}"
+  echo "Stack trace, excluding subshells:"
+  local frame=0
+  while caller ${frame}; do
+    frame=$((frame + 1))
+  done
+}
+
+trap exit_handler EXIT
+trap err_handler ERR
+
+function base_status {
+  local field
+  field="`echo $1 | cut -d , -f 6`" || exit
+  if test x"${field}" = xS; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function name {
+  echo $1 | cut -d , -f 1 || exit
+}
+
+function base_runtime {
+  echo $1 | cut -d , -f 3 || exit
+}
+
+function base_ratio {
+  echo $1 | cut -d , -f 4 || exit
+}
+
+#TODO if there is a base and peak run, we should report both sides FUTURE WORK
+#TODO pass other useful data as attributes - e.g. workload, other data from the csv file
+run="$1"
+if ! test -d "${run}"; then
+  echo "Directory of runs of benchmark script ($1) not found, or is not a directory" >&2
+  exit 1
+fi
+if test "`ls ${run}/result/CINT2006.*.*.csv 2>/dev/null | wc -l`" -gt 1 ||
+   test "`ls ${run}/result/CFP2006.*.*.csv  2>/dev/null | wc -l`" -gt 1; then
+  echo "Multiple runs of SPEC unsupported" >&2
+  exit 1
+fi
+if test "`ls ${run}/result/CINT2006.*.*.csv 2>/dev/null | wc -l`" -ne 1 &&
+   test "`ls ${run}/result/CFP2006.*.*.csv  2>/dev/null | wc -l`" -ne 1; then
+  echo "No runs of SPEC!" >&2
+  exit 1
+fi
+
+for csv in `ls ${run}/result/C{INT,FP}2006.*.*.csv 2>/dev/null`; do
+  #data about the run
+  run_set="`basename ${csv} | cut -d . -f 1`" || exit #CINT2006 or CFP2006
+  run_index=$((`basename ${csv} | cut -d . -f 2` - 1)) || exit #Always 1 so long as we aren't supporting multiple runs of SPEC
+  run_workload="`basename ${csv} | cut -d . -f 3`" || exit #test, train, ref
+
+  #slurp results file
+  line=("")
+  while IFS='' read -r l; do line=("${line[@]}" "${l}"); done < "${csv}"
+  line_max=$((${#line[@]} - 1))
+
+  #submit full results
+  for i in `seq 0 ${line_max}`; do
+    if test x"${line[$i]}"x = x'"Full Results Table"'x; then break; fi
+  done
+
+  iteration=0
+  oldname=""
+  for i in `seq $((i + 2)) ${line_max}`; do
+    if test x"${line[$i]}"x = xx; then break; fi
+    name="`name ${line[$i]}`"
+    runtime="`base_runtime ${line[$i]}`"
+    ratio="`base_ratio ${line[$i]}`"
+
+    if test x"${name}" = x"${oldname}"; then
+      iteration=$((iteration + 1))
+    else
+      iteration=1
+    fi
+
+    if base_status "${line[$i]}"; then
+      ltc "${name}[${iteration}]" \
+        --result pass --measurement "${runtime}" --units seconds
+      if test x"${ratio}" != x; then
+        ltc "${name}[${iteration}]" \
+          --result pass --measurement "${ratio}" --units ratio
+      fi
+    else
+      ltc "${name}[${iteration}]" --result fail
+    fi
+
+    oldname="${name}"
+  done
+
+  #submit selected results
+  for i in `seq $((i + 1)) ${line_max}`; do
+    if test x"${line[$i]}"x = x'"Selected Results Table"'x; then break; fi
+  done
+
+  count=0
+  base_runtime_product=1
+  for i in `seq $((i + 2)) ${line_max}`; do
+    if test x"${line[$i]}"x = xx; then break; fi
+    name="`name ${line[$i]}`"
+    runtime="`base_runtime ${line[$i]}`"
+    ratio="`base_ratio ${line[$i]}`"
+    if base_status "${line[$i]}"; then
+      count=$((count + 1))
+      base_runtime_product="`echo \"${base_runtime_product} * ${runtime}\" | bc`" || exit
+      ltc "${name}" --result pass \
+        --measurement "${runtime}" --units seconds
+      if test x"${ratio}" != x; then
+        ltc "${name}" --result pass \
+          --measurement "${ratio}" --units ratio
+      fi
+    else
+      ltc "${name}" --result fail
+    fi
+  done
+
+  #compute and report geomean of runtime
+  if test ${count} -ne 0; then
+    ltc "${run_set} base runtime geomean" --result pass \
+      --measurement "`echo \"scale=6; e(l(${base_runtime_product})/${count})\" | bc -l`" \
+      --units 'geomean of selected runtimes (seconds)' || exit
+  else
+    ltc "${run_set}" --result fail
+  fi
+
+  #if we have ratios, we will have a score to report
+  if test x"`base_ratio ${line[$i-1]}`" != x; then
+    i=$((i + 1))
+    score="`echo ${line[$i]} | cut -d , -f 2`" || exit
+    ltc "${run_set} base score" --result pass \
+      --measurement "${score}" --units 'base score (geomean of selected ratios)'
+  fi
+done
+
+error=0
---
+++ config/bench/gather/test_CPU2006.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -ue
+
+function doit {
+  min_mult=$((RANDOM % 5 + 95)) #95 - 99
+  max_mult=$((RANDOM % 5 + 1)) #1 - 5
+
+  median=${RANDOM}.${RANDOM}
+  min=`echo "scale=6; ${median} * 0.${min_mult}" | bc -l`
+  max=`echo "scale=6; ${median} * 1.0${max_mult}" | bc -l`
+
+  runtime=(${min} ${median} ${max})
+  base_multiplier=$((RANDOM % 25 + 25)) #25 - 49
+  base=`echo "($median * $base_multiplier) / 1" | bc`
+
+  count=1
+  for i in `echo -e "0\\n1\\n2" | sort -R`; do
+    ratio=`echo "${base}/${runtime[$i]}" | bc -l`
+    line="`printf '%s,%i,%f,%f,%i,S,,,,,,ref iteration #%i\n' \
+      $1 \
+      $base \
+      ${runtime[$i]} \
+      ${ratio} \
+      $((i % 2)) \
+      ${count}`"
+    testcase=("${testcase[@]}" \
+      "`printf 'lava-test-case %s[%i] --result pass --measurement %f --units %s\n' \
+        $1 ${count} ${runtime[$i]} 'seconds'`" \
+      "`printf 'lava-test-case %s[%i] --result pass --measurement %f --units %s\n' \
+        $1 ${count} ${ratio} 'ratio'`"
+      )
+    if test $((i%2)) -eq 1; then
+      selected=("${selected[@]}" "$line")
+      testcase_selected=("${testcase_selected[@]}" \
+      "`printf 'lava-test-case %s --result pass --measurement %f --units %s\n' \
+        $1 ${runtime[$i]} 'seconds'`" \
+      "`printf 'lava-test-case %s --result pass --measurement %f --units %s\n' \
+        $1 ${ratio} 'ratio'`"
+      )
+      selected_count=$((selected_count + 1))
+      selected_product_runtime=`echo "${selected_product_runtime} * ${runtime[$i]}" | bc -l`
+      selected_product_ratio=`echo "${selected_product_ratio} * ${ratio}" | bc -l`
+    fi
+    count=$((count + 1))
+    echo $line
+  done
+}
+
+declare -A names
+names['fp']='410.bwaves 416.gamess 433.milc 434.zeusmp 435.gromacs 436.cactusADM 437.leslie3d 444.namd 447.dealII 450.soplex 453.povray 454.calculix 459.GemsFDTD 465.tonto 470.lbm 481.wrf 482.sphinx3'
+names['int']='400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar 483.xalancbmk'
+rm -rf testing
+mkdir -p testing/input/result
+for bset in fp int; do
+  exec {STDOUT}>&1
+  exec 1>testing/input/result/C${bset^^}2006.1.test.csv
+  echo '"Full Results Table"'
+  echo
+  selected=('"Selected Results Table"' '')
+  testcase=('')
+  testcase_selected=('')
+  selected_count=0
+  selected_product_runtime=1
+  selected_product_ratio=1
+  for name in ${names[${bset}]}; do
+    doit $name
+  done
+  echo
+  for x in "${selected[@]}"; do
+    echo "$x"
+  done
+  echo
+  score=`echo "scale=6; e(l(${selected_product_ratio})/${selected_count})" | bc -l`
+  echo "SPEC${bset}_base2006,${score},,${score}"
+
+  for x in "${testcase[@]:1}"; do
+    echo "$x" >> testing/golden
+  done
+  for x in "${testcase_selected[@]:1}"; do
+    echo "$x" >> testing/golden
+  done
+  printf 'lava-test-case %s --result pass --measurement %f --units %s\n' \
+    "C${bset^^}2006 base runtime geomean" \
+    `echo "scale=6; e(l(${selected_product_runtime})/${selected_count})" | bc -l` \
+    'geomean of selected runtimes (seconds)' >> testing/golden
+  printf 'lava-test-case %s --result pass --measurement %f --units %s\n' \
+    "C${bset^^}2006 base score" \
+    ${score} \
+    'base score (geomean of selected ratios)' >> testing/golden
+  exec 1>&${STDOUT}
+done
+
+TESTING=1 ./CPU2006.sh testing/input > testing/output
+
+if diff testing/{golden,output}; then
+  echo "CPU2006 gather script passed self-tests"
+  exit 0
+else
+  echo "CPU2006 gather script FAILED self-tests"
+  exit 1
+fi
