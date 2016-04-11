c814168b083be4570a6b492b7787e81d88b7c005
Christophe Lyon <christophe.lyon@linaro.org>
2016-04-11 21:58:55 +0200

    Handle multiple dbg.log files.
    Change-Id: Icd889670f17c81d671fbdf194b637a6f5bd06655

--- jenkins.sh
+++ jenkins.sh
@@ -479,9 +479,9 @@ if test x"${logserver}" != x"" && test x"${sums}" != x -o x"${runtests}" != x"tr
     cp ${logs} ${logs_dir}/ || status=1
 
     # Copy over the runtest debug logs
-    logs="`find ${user_workspace}/_build/builds/*/*/gcc.git*-stage2  -name dbg.log`"
+    dbglogs="`find ${user_workspace}  -name dbg.log`"
 
-    cp ${logs} ${logs_dir}/ || status=1
+    cat ${dbglogs} ${logs_dir}/ || status=1
 
     # Copy stdout and stderr output from abe.
     cp build.out build.err ${logs_dir}/ || status=1
