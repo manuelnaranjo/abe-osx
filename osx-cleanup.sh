3d2cd16b49f93fc1f1afe2917df436e2d0155471
Manuel Naranjo <mnaranjo@irobot.com>
2016-06-09 15:01:34 -0300

    [osx] Adding init and cleanup script
    This scripts are needed to addapt the brew setup in order to make the
    system build correctly.

---
+++ osx-cleanup.sh
@@ -0,0 +1,17 @@
+rm -f /usr/local/bin/md5sum
+rm -f /usr/local/bin/readlink
+rm -f /usr/local/bin/test
+rm -f /usr/local/bin/sed
+rm -f /usr/local/bin/cut
+rm -f /usr/local/bin/stat
+rm -f /usr/local/bin/ln
+
+rm -f /usr/local/bin/c++
+rm -f /usr/local/bin/cpp
+rm -f /usr/local/bin/g++
+rm -f /usr/local/bin/gcc
+# rm -f /usr/local/bin/ar
+# rm -f /usr/local/bin/nm
+# rm -f /usr/local/bin/ranlib
+rm -f /usr/local/bin/gconv
+rm -f /usr/local/bin/gconv-tool
---
+++ osx-prepare.sh
@@ -0,0 +1,17 @@
+cp -f `which gmd5sum` /usr/local/bin/md5sum
+cp -f `which greadlink` /usr/local/bin/readlink
+cp -f `which gtest` /usr/local/bin/test
+cp -f `which gsed` /usr/local/bin/sed
+cp -f `which gcut` /usr/local/bin/cut
+cp -f `which gstat` /usr/local/bin/stat
+cp -f `which gln` /usr/local/bin/ln
+
+ln -s `which c++-5` /usr/local/bin/c++
+ln -s `which cpp-5` /usr/local/bin/cpp
+ln -s `which g++-5` /usr/local/bin/g++
+ln -s `which gcc-5` /usr/local/bin/gcc
+#ln -s `which gcc-ar-5` /usr/local/bin/ar
+#ln -s `which gcc-nm-5` /usr/local/bin/nm
+#ln -s `which gcc-ranlib-5` /usr/local/bin/ranlib
+ln -s `which gcov-5` /usr/local/bin/gcov
+ln -s `which gcov-tool-5` /usr/local/bin/gcov-tool
